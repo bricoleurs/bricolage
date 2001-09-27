5045483509547cb81fa4d8e58a087367446496ee
David Wheeler <david@justatheory.com>
2001-09-27 12:09:16 +0000

    Added ORDER BY clause to the SELECT statement.

--- lib/Bric/Biz/ATType.pm
+++ lib/Bric/Biz/ATType.pm
@@ -7,15 +7,15 @@ Bric::Biz::ATType - A class to represent AssetType types.
 
 =head1 VERSION
 
-$Revision: 1.2 $
+$Revision: 1.3 $
 
 =cut
 
-our $VERSION = substr(q$Revision: 1.2 $, 10, -1);
+our $VERSION = substr(q$Revision: 1.3 $, 10, -1);
 
 =head1 DATE
 
-$Date: 2001-09-06 22:30:06 $
+$Date: 2001-09-27 12:09:16 $
 
 =head1 SYNOPSIS
 
@@ -791,6 +791,7 @@ sub _select_attype {
 
     my $sql = 'SELECT '.join(',',@cols).' FROM '.TABLE;
     $sql   .= ' WHERE '.$where if $where;
+    $sql   .= ' ORDER BY name';
 
     my $sth = prepare_ca($sql, undef, DEBUG);
 
@@ -869,7 +870,10 @@ L<perl>, L<Bric>, L<Bric::Biz::AssetType>
 =head1 REVISION HISTORY
 
 $Log: ATType.pm,v $
-Revision 1.2  2001-09-06 22:30:06  samtregar
+Revision 1.3  2001-09-27 12:09:16  wheeler
+Added ORDER BY clause to the SELECT statement.
+
+Revision 1.2  2001/09/06 22:30:06  samtregar
 Fixed remaining BL->App, BC->Biz conversions
 
 Revision 1.1.1.1  2001/09/06 21:53:05  wheeler
