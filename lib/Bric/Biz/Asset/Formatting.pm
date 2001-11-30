6b4217380c7808a4fd0c9c9959fb423959f48e09
David Wheeler <david@justatheory.com>
2001-11-30 06:05:37 +0000

    Added '.mc' to the file name. Sam, you'll no doubt want to change this in HEAD.

--- lib/Bric/Biz/Asset/Formatting.pm
+++ lib/Bric/Biz/Asset/Formatting.pm
@@ -7,15 +7,15 @@ Bric::Biz::Asset::Formatting - AN object housing the formatting Assets
 
 =head1 VERSION
 
-$Revision: 1.4.2.5 $
+$Revision: 1.4.2.6 $
 
 =cut
 
-our $VERSION = (qw$Revision: 1.4.2.5 $ )[-1];
+our $VERSION = (qw$Revision: 1.4.2.6 $ )[-1];
 
 =head1 DATE
 
-$Date: 2001-11-30 06:00:14 $
+$Date: 2001-11-30 06:05:37 $
 
 =head1 SYNOPSIS
 
@@ -2140,6 +2140,7 @@ $build_file_name = sub {
 
     # Add the name.
     (my $file = $self->_get('name')) =~ s/\W+/_/g;
+    $file .= '.mc' unless $name eq 'autohandler';
 
     # Return the filename.
     return Bric::Util::Trans::FS->cat_uri(@path, $file);
