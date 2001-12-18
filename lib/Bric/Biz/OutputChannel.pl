669aadce486966a66ffa5f9e6c06c59e1e89bb61
David Wheeler <david@justatheory.com>
2001-12-18 03:20:42 +0000

    Rewritten to use true testing syntax. Tests OutputChannel class more thoroughly now.

--- lib/Bric/Biz/OutputChannel.pl
+++ lib/Bric/Biz/OutputChannel.pl
@@ -1,49 +1,64 @@
 #!/usr/bin/perl
 
-use Bric::BC::OutputChannel;
-
 use strict;
-
-$Bric::Cust = 'mike';
+use Test;
+BEGIN { plan tests => 29 };
+use Bric::Biz::OutputChannel;
+ok(1); # If we made it this far, we're ok.
 
 my $param = {
-	'name'			=> 'mike\'s test5',
-	'description'	=> 'a fun test',
-	'tile_aware'	=> 1,
-	'primary'		=> 1,
-	'active'		=> 1
+	     name        => 'mike\'s test5',
+	     description => 'a fun test',
+	     tile_aware  => 1,
+	     primary     => 1,
+	     pre_path    => 'foo',
+	     post_path   => 'en',
+	     active      => 1
 };
 
+# Construct a new OC.
+ok( my $oc = Bric::Biz::OutputChannel->new($param) );
+ok( $oc->save );
+ok( my $id = $oc->get_id );
+
+# Okay, now lookup an existing one.
+ok( $oc = Bric::Biz::OutputChannel->lookup({ id => $id }) );
+ok( $oc->get_id == $id);
+
+# Test all the instance methods.
+ok( my $id = $oc->get_id );
+ok( $oc->get_name eq 'mike\'s test5' );
+ok( $oc->set_name('Another test!!!') );
+ok( $oc->get_description eq 'a fun test');
+ok( $oc->set_description('fun, fun, fun!') );
+
+ok( $oc->get_primary );
+ok( $oc->set_primary(undef) );
+ok( !$oc->get_primary );
+ok( $oc->set_primary(1) );
+ok( $oc->get_primary );
+
+ok( $oc->get_pre_path eq 'foo' );
+ok( $oc->set_pre_path('bar') );
+ok( $oc->get_post_path eq 'en');
+ok( $oc->set_post_path('fr') );
+
+ok( $oc->is_active );
+ok( $oc->deactivate );
+ok( !$oc->is_active );
+ok( $oc->activate );
+ok( $oc->is_active );
+ok( $oc->save );
 
 
-my $oc = Bric::BC::OutputChannel->new($param);
+# Okay, now get a list.
+ok( my @ocs = Bric::Biz::OutputChannel->list({ name => 'Another test!!!' }) );
+ok( $ocs[0]->get_id );
 
-$oc->save();
+# Now clean up.
+Bric::Util::DBI::prepare(qq{
+    DELETE FROM output_channel
+    WHERE  id = $id
+})->execute;
 
-my $id = $oc->get_id();
-
-print "OC $id created\n";
-
-$oc = undef;
-
-$oc = Bric::BC::OutputChannel->lookup( { id => $id } );
-
-my $name = $oc->get_name();
-
-print "OC of name = $name has been looked up!\n\n";
-
-$oc = undef;
-
-delete $param->{'description'};
-
-my @ocs = Bric::BC::OutputChannel->list($param);
-
-print "list returned these objects\n";
-
-foreach (@ocs) {
-	$id = $_->get_id();
-	print "\tID of $id\n";
-	$_->set_description('a new description');
-	$_->save();
-}
-
+ok(1); # Success!
