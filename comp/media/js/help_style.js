d4efd0fc9587818babfa29edcfffc3945841c187
David Wheeler <david@justatheory.com>
2002-01-11 00:48:20 +0000

    Tweaked typography for Windows and Linux.

--- comp/media/js/help_style.js
+++ comp/media/js/help_style.js
@@ -2,15 +2,16 @@ if (is_nav6up) {
     var ss = document.styleSheets[0];
     var i = 0;
     ss.insertRule("body { background:white; font-family:Verdana,Helvetica,Arial,sans-serif; }", i++);
-    ss.insertRule(".tab { background:#006666; color:white; font-weight:bold; font-size:11pt; }", i++);
-    ss.insertRule(".light { background:#cccc99; font-size:9.5pt; }", i++);
-    ss.insertRule(".context { background:#999966; font-size:9.5pt; line-height:12pt; }", i++);
-    ss.insertRule("h3 { font-size:11pt; line-height:13pt; }", i++);
-    ss.insertRule("p { font-size:10pt; line-height:13pt; }", i++);
+    ss.insertRule(".tab { background:#006666; color:white; font-weight:bold; font-size:10pt; }", i++);
+    ss.insertRule(".light { background:#cccc99; font-size:7pt; }", i++);
+    ss.insertRule(".context { background:#999966; font-size:7.5pt; line-height:12pt; }", i++);
+    ss.insertRule("h3 { font-size:10pt; line-height:13pt; }", i++);
+    ss.insertRule("p { font-size:8.5pt; line-height:12pt; }", i++);
     ss.insertRule(".redLink { color:#993300; }", i++);
-    ss.insertRule("td { font-size:10pt; line-height:13pt; }", i++);
-    ss.insertRule("dt {  font-size:10pt; line-height:14pt; font-weight:bold; }", i++);
-    ss.insertRule("dd { font-size:10pt; line-height:13pt; }", i++);
+    ss.insertRule("td { font-size:8.5pt; line-height:12pt; }", i++);
+    ss.insertRule("dt {  font-size:8.5pt; line-height:13pt; font-weight:bold; }", i++);
+    ss.insertRule("dd { font-size:8.5pt; line-height:12pt; }", i++);
+    ss.insertRule("li { font-size:8.5pt; line-height:12pt; }", i++);
 } else if (is_nav4up) {
     var ss = document.classes;
     var tags = document.tags;
@@ -21,7 +22,7 @@ if (is_nav6up) {
     var lgHeight = '16pt';
     if (is_win) {
         size = '9pt';
-        height = '12pt';
+        height = '13pt';
         smSize = '8pt';
         lgSize = '10pt';
         lgHeight = '13pt';
@@ -46,6 +47,10 @@ if (is_nav6up) {
     tags.dl.lineHeight = height;
     tags.dt.fontWeight = 'bold';
 
+    tags.ul.fontFamily = 'Verdana,Helvetica,Arial,sans-serif';
+    tags.ul.fontSize = size;
+    tags.ul.lineHeight = height;
+
     ss.tab.all.fontFamily = 'Verdana,Helvetica,Arial,sans-serif';
     ss.tab.all.fontWeight = 'bold';
     ss.tab.all.fontSize = lgSize;
@@ -67,12 +72,14 @@ if (is_nav6up) {
     ss.addRule(".redLink", "color:#993300;", i++);
     if (is_win) {
         ss.addRule("body", "background:white; font-family:Verdana,Helvetica,Arial,sans-serif;", i++);
+        ss.addRule(".light", "background:#cccc99; font-size:7pt;", i++);
         ss.addRule(".tab", "background:#006666; color:white; font-weight:bold; font-size:10pt;", i++);
         ss.addRule(".context", "background:#999966; font-size:7.25pt; line-height:9pt;", i++);
         ss.addRule("h3", "font-size:10pt; line-height:12pt;", i++);
         ss.addRule("p", "font-size:8pt; line-height:11pt;", i++);
         ss.addRule("dt", " font-size:8pt; line-height:11pt; font-weight:bold;", i++);
         ss.addRule("dd", "font-size:8pt; line-height:11pt;", i++);
+        ss.addRule("li", "font-size:8pt; line-height:11pt;", i++);
     } else if (is_mac) {
         ss.addRule("body", "background:white; font-family:Verdana,Helvetica,Arial,sans-serif;", i++);
         ss.addRule(".tab", "background:#006666; color:white; font-weight:bold; font-size:11pt;", i++);
@@ -81,6 +88,7 @@ if (is_nav6up) {
         ss.addRule("p", "font-size:7.5pt; line-height:10.5pt;", i++);
         ss.addRule("dt", " font-size:7.5pt; line-height:10.5pt; font-weight:bold;", i++);
         ss.addRule("dd", "font-size:7.5pt; line-height:10.5pt;", i++);
+        ss.addRule("li", "font-size:7.5pt; line-height:10.5pt;", i++);
     }
 } else {
     // It's something else.
