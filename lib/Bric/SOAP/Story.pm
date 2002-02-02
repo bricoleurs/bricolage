15f598b651fd8761c50c5f669a3d11123be1bc0e
Sam Tregar <sam@tregar.com>
2002-02-02 00:12:53 +0000

    Feature-complete Bric::SOAP::Story.  Added update() and tests.

--- lib/Bric/SOAP/Story.pm
+++ lib/Bric/SOAP/Story.pm
@@ -9,8 +9,8 @@ use Bric::Biz::AssetType;
 use Bric::Biz::Category;
 use Bric::Util::Grp::Parts::Member::Contrib;
 use Bric::Biz::Workflow qw(STORY_WORKFLOW);
-use Bric::App::Session qw(:user);
-use Bric::App::Authz qw(chk_authz READ EDIT CREATE);
+use Bric::App::Session  qw(get_user_id);
+use Bric::App::Authz    qw(chk_authz READ EDIT CREATE);
 use XML::Writer;
 use IO::Scalar;
 use Carp qw(croak);
@@ -35,15 +35,15 @@ Bric::SOAP::Story - SOAP interface to Bricolage stories.
 
 =head1 VERSION
 
-$Revision: 1.9 $
+$Revision: 1.10 $
 
 =cut
 
-our $VERSION = (qw$Revision: 1.9 $ )[-1];
+our $VERSION = (qw$Revision: 1.10 $ )[-1];
 
 =head1 DATE
 
-$Date: 2002-02-01 00:52:23 $
+$Date: 2002-02-02 00:12:53 $
 
 =head1 SYNOPSIS
 
@@ -411,7 +411,7 @@ Available options:
 
 =item document (required)
 
-The XML document containing objects to be createed.  The document must
+The XML document containing objects to be created.  The document must
 contain at least one story and may contain any number of related media
 objects.
 
@@ -445,189 +445,14 @@ sub create {
     }
 
     # make sure we have a document
-    my $document = $args->{document};
     die __PACKAGE__ . "::create : missing required document parameter.\n"
-      unless $document;
+      unless $args->{document};
 
-    # parse and catch erros
-    my $data;
-    eval { $data = parse_asset_document($document) };
-    die __PACKAGE__ . "::create : problem parsing asset document : $@\n"
-	if $@;
-    die __PACKAGE__ . "::create : problem parsing asset document : no stories found!\n"
-	unless ref $data and ref $data eq 'HASH' and exists $data->{story};
-    print STDERR Data::Dumper->Dump([$data],['data']) if DEBUG;
+    # setup empty update_ids arg to indicate create state
+    $args->{update_ids} = [];
 
-    # loop over stories, filling in %story_ids and @relations
-    my (%story_ids, @story_ids, @relations);
-    foreach my $sdata (@{$data->{story}}) {
-	my %init;
-
-	# get user__id from Bric::App::Session
-	$init{user__id} = get_user_id;
-	
-	# get element__id from story element
-	($init{element__id}) = Bric::Biz::AssetType->list_ids(
-                                 { name => $sdata->{element} });
-	die __PACKAGE__ . "::create : no element found matching " .
-	    "(element => \"$sdata->{element}\")\n"
-		unless defined $init{element__id};
-
-	# get source__id from source
-	($init{source__id}) = Bric::Biz::Org::Source->list_ids(
-                                 { source_name => $sdata->{source} });
-	die __PACKAGE__ . "::create : no source found matching " .
-	    "(source => \"$sdata->{source}\")\n"
-		unless defined $init{source__id};
-
-	# create empty story
-	my $story = Bric::Biz::Asset::Business::Story->new(\%init);
-	die __PACKAGE__ . "::create : failed to create empty story object."
-	    unless $story;
-	print STDERR __PACKAGE__ . "::create : created empty story object\n"
-	    if DEBUG;
-
-	# is this is right way to check create access for stories?
-	die __PACKAGE__ . "::create : access denied.\n"
-	    unless chk_authz($story, CREATE, 1);
-
-	# set simple fields
-	my @simple_fields = qw(name description slug primary_uri
-			       priority publish_status);
-	$story->_set(\@simple_fields, [ @{$sdata}{@simple_fields} ]);
-
-	# activate?
-	$sdata->{active} ? $story->activate : $story->deactivate;
-
-	# assign dates 
-	for my $name qw(cover_date expire_date publish_date) {
-	    my $date = $sdata->{$name};
-	    next unless $date; # skip missing date
-	    my $pg_date = xs_date_to_pg_date($date);
-	    die __PACKAGE__ . "::export : bad date format for $name : $date\n"
-		unless defined $pg_date;
-	    $story->_set([$name],[$pg_date]);
-	}
-
-	# assign categories
-	my @cids;
-	my $primary_cid;
-	foreach my $cdata (@{$sdata->{categories}{category}}) {
-	    # get cat id
-	    my $path = ref $cdata ? $cdata->{content} : $cdata;
-	    my $category_id = category_path_to_id($path);
-	    die __PACKAGE__ . "::create : no category found matching " .
-		"(category => \"$path\")\n"
-		    unless defined $category_id;
-	    
-	    push(@cids, $category_id);
-	    $primary_cid = $category_id 
-		if ref $cdata and $cdata->{primary};
-	}
-	
-	# sanity checks
-	die __PACKAGE__ . "::create : no categories defined!"
-	    unless @cids;
-	die __PACKAGE__ . "::create : no primary category defined!"
-	    unless defined $primary_cid;
-	    
-	# add categories to story
-	$story->add_categories(\@cids);	
-	$story->set_primary_category($primary_cid);
-
-
-	# add keywords, if we have any
-	if ($sdata->{keywords} and $sdata->{keywords}{keyword}) {
-
-	    # collect keyword objects
-	    my @kws;
-	    foreach (@{$sdata->{keywords}{keyword}}) {
-		my $kw = Bric::Biz::Keyword->lookup({ name => $_ });
-		$kw ||= Bric::Biz::Keyword->new({ name => $_})->save;
-		push @kws, $kw;
-	    }
-
-	    # add keywords to the story
-	    $story->add_keywords(\@kws);
-	}
-
-	# add contributors, if any
-	if ($sdata->{contributors} and $sdata->{contributors}{contributor}) {
-	    foreach my $c (@{$sdata->{contributors}{contributor}}) {
-		my %init = (fname => $c->{fname}, 
-			    mname => $c->{mname},
-			    lname => $c->{lname});
-		my ($contrib) = 
-		    Bric::Util::Grp::Parts::Member::Contrib->list(\%init);
-		die __PACKAGE__ . "::create : no contributor found matching " .
-		    "(contributer => " . 
-			join(', ', map { "$_ => $c->{$_}" } keys %$c)
-		    unless defined $contrib;
-		$story->add_contributor($contrib, $c->{role});
-	    }
-	}
-
-	# save the story in an inactive state.  this is necessary to
-	# allow element addition - you can't add elements to an
-	# unsaved story, strangely.
-	$story->deactivate;
-	$story->save;
-
-	# find a suitable workflow and desk for the story.  Might be
-	# nice if Bric::Biz::Workflow->list took a type key...
-	my $desk;
-	foreach my $workflow (Bric::Biz::Workflow->list()) {
-	    if ($workflow->get_type == STORY_WORKFLOW) {
-		$story->set_workflow_id($workflow->get_id());
-		$desk = $workflow->get_start_desk;
-		$desk->accept({'asset' => $story});
-		last;
-	    }
-	}
-
-	# add element data
-	if ($sdata->{elements}) {
-	    $pkg->_load_element(element   => $story->get_tile, 
-				data      => $sdata->{elements},
-			        relations => \@relations);
-	} else {
-	    # load an empty set to create a really empty story
-	    $pkg->_load_element(element   => $story->get_tile, 
-				data      => {});
-	}
-
-	# save the story and desk after activating if desired
-	$story->activate if $sdata->{active};
-	$desk->save;
-	$story->save;
-
-	# this is what story_prof does - skipping this step results in
-	# a story unstuck in time with version set to 0
-	$story->checkin();
-	$story->save();
-	$story->checkout( { user__id => get_user_id });
-	$story->save();
-
-	# all done, setup the story_id
-	push(@story_ids, $story_ids{$sdata->{id}} = $story->get_id);
-    }
-
-    # resolve relations
-    # FIX: more checking here would be nice
-    foreach my $r (@relations) {
-	if (defined $r->{story_id}) {
-	    if ($r->{relative}) {
-		$r->{container}->set_related_instance_id($story_ids{$r->{story_id}});
-	    } else {
-		$r->{container}->set_related_instance_id($r->{story_id});
-	    }
-	} else {
-	    # FIX: do related media stuff when Bric::SOAP::Media is done
-	}
-	$r->{container}->save;
-    }
-    
-    return name(ids => [ map { name(id => $_) } @story_ids ]);
+    # call _load_stories
+    return $pkg->_load_stories($args);
 }
 }
 
@@ -638,7 +463,8 @@ of the format created by export().  A common use of update() is to
 export() a selected story, make changes to one or more fields and
 then submit the changes with update(). 
 
-Returns "1" on success, "0" on failure.
+Returns a list of new ids created in the order of the assets in the
+document.
 
 Takes the following options:
 
@@ -650,10 +476,13 @@ The XML document where the objects to be updated can be found.  The
 document must contain at least one story and may contain any number of
 related media objects.
 
-=item ids (required)
+=item update_ids (required)
 
-A list of "id" integers for the assets to be updated.  You must
-provide exactly as many ids as there are assets in your document.
+A list of "story_id" integers for the assets to be updated.  These
+must match id attributes on story elements in the document.  If you
+include objects in the document that are not listed in update_ids then
+they will be treated as in create().  For that reason an update() with
+an empty update_ids list is equivalent to a create().
 
 =back 4
 
@@ -662,6 +491,45 @@ Throws: NONE
 Side Effects: NONE
 
 Notes: NONE
+
+=cut
+
+# hash of allowed parameters
+{
+my %allowed = map { $_ => 1 } qw(document update_ids);
+
+sub update {
+    my $pkg = shift;
+    our $ef;
+    my $env = pop;
+    my $args = $env->method || {};    
+    
+    print STDERR __PACKAGE__ . "->update() called : args : ", 
+      Data::Dumper->Dump([$args],['args']) if DEBUG;
+    
+    # check for bad parameters
+    for (keys %$args) {
+	die __PACKAGE__ . "::update : unknown parameter \"$_\".\n"
+	    unless exists $allowed{$_};
+    }
+
+    # make sure we have a document
+    die __PACKAGE__ . "::update : missing required document parameter.\n"
+      unless $args->{document};
+
+    # make sure we have an update_ids array
+    die __PACKAGE__ . "::update : missing required update_ids parameter.\n"
+      unless $args->{update_ids};
+    die __PACKAGE__ . 
+	"::update : malformed update_ids parameter - must be an array.\n"
+	    unless ref $args->{update_ids} and 
+                   ref $args->{update_ids} eq 'ARRAY';
+
+    # call _load_stories
+    return $pkg->_load_stories($args);
+}
+}
+
 
 =item delete
 
@@ -752,6 +620,252 @@ sub delete {
 =head2 Private Class Methods
 
 =over 4
+
+=item $pkg->_load_stories($args)
+
+This method provides the meat of both create() and update().  The only
+difference between the two methods is that update_ids will be empty on
+create().
+
+=cut
+
+sub _load_stories {
+    my ($pkg, $args) = @_;
+    my $document = $args->{document};
+    my %to_update = map { $_ => 1 } @{$args->{update_ids}};
+
+    # parse and catch erros
+    my $data;
+    eval { $data = parse_asset_document($document) };
+    die __PACKAGE__ . " : problem parsing asset document : $@\n"
+	if $@;
+    die __PACKAGE__ . " : problem parsing asset document : no stories found!\n"
+	unless ref $data and ref $data eq 'HASH' and exists $data->{story};
+    print STDERR Data::Dumper->Dump([$data],['data']) if DEBUG;
+
+    # loop over stories, filling in %story_ids and @relations
+    my (%story_ids, @story_ids, @relations);
+    foreach my $sdata (@{$data->{story}}) {
+	my $id = $sdata->{id};
+
+	# are we updating?
+	my $update = exists $to_update{$id};	
+
+	# setup init data for create
+	my %init;
+
+	# get user__id from Bric::App::Session
+	$init{user__id} = get_user_id;
+	
+	# get element__id from story element
+	($init{element__id}) = Bric::Biz::AssetType->list_ids(
+                                 { name => $sdata->{element} });
+	die __PACKAGE__ . "::create : no element found matching " .
+	    "(element => \"$sdata->{element}\")\n"
+		unless defined $init{element__id};
+
+	# get source__id from source
+	($init{source__id}) = Bric::Biz::Org::Source->list_ids(
+                                 { source_name => $sdata->{source} });
+	die __PACKAGE__ . "::create : no source found matching " .
+	    "(source => \"$sdata->{source}\")\n"
+		unless defined $init{source__id};
+
+	# get base story object
+	my $story;
+	unless ($update) {
+	    # create empty story
+	    $story = Bric::Biz::Asset::Business::Story->new(\%init);
+	    die __PACKAGE__ . "::create : failed to create empty story object."
+		unless $story;
+	    print STDERR __PACKAGE__ . "::create : created empty story object\n"
+		if DEBUG;
+
+	    # is this is right way to check create access for stories?
+	    die __PACKAGE__ . " : access denied.\n"
+		unless chk_authz($story, CREATE, 1);
+
+	} else {
+	    # updating - first look for a checked out version
+	    $story = Bric::Biz::Asset::Business::Story->lookup({ id => $id,
+								 checkout => 1
+							       });
+	    if ($story) {
+		# make sure it's ours
+		die __PACKAGE__ . 
+		    "::update : story \"$id\" is checked out to another user.\n"
+			unless $story->get_user__id == get_user_id;
+		die __PACKAGE__ . " : access denied.\n"
+		    unless chk_authz($story, CREATE, 1);
+	    } else {
+		# try a non-checked out version
+		$story = Bric::Biz::Asset::Business::Story->lookup({ id => $id });
+		die __PACKAGE__ . "::update : no story found for \"$id\"\n"
+		    unless $story;
+		die __PACKAGE__ . " : access denied.\n"
+		    unless chk_authz($story, CREATE, 1);
+
+	        # FIX: race condition here - between lookup and checkout 
+                #      someone else could checkout...
+
+		# check it out 
+		$story->checkout( { user__id => get_user_id });
+		$story->save();
+	    }
+
+	    # update %init fields
+	    $story->set_element__id($init{element__id});
+	    $story->set_source__id($init{source__id});    
+	}
+
+	# set simple fields
+	my @simple_fields = qw(name description slug primary_uri
+			       priority publish_status);
+	$story->_set(\@simple_fields, [ @{$sdata}{@simple_fields} ]);
+
+	# assign dates 
+	for my $name qw(cover_date expire_date publish_date) {
+	    my $date = $sdata->{$name};
+	    next unless $date; # skip missing date
+	    my $pg_date = xs_date_to_pg_date($date);
+	    die __PACKAGE__ . "::export : bad date format for $name : $date\n"
+		unless defined $pg_date;
+	    $story->_set([$name],[$pg_date]);
+	}
+
+	# remove all categories if updating
+	$story->delete_categories([ $story->get_categories ])
+	    if $update;
+
+	# assign categories
+	my @cids;
+	my $primary_cid;
+	foreach my $cdata (@{$sdata->{categories}{category}}) {
+	    # get cat id
+	    my $path = ref $cdata ? $cdata->{content} : $cdata;
+	    my $category_id = category_path_to_id($path);
+	    die __PACKAGE__ . "::create : no category found matching " .
+		"(category => \"$path\")\n"
+		    unless defined $category_id;
+	    
+	    push(@cids, $category_id);
+	    $primary_cid = $category_id 
+		if ref $cdata and $cdata->{primary};
+	}
+	
+	# sanity checks
+	die __PACKAGE__ . "::create : no categories defined!"
+	    unless @cids;
+	die __PACKAGE__ . "::create : no primary category defined!"
+	    unless defined $primary_cid;
+	    
+	# add categories to story
+	$story->add_categories(\@cids);	
+	$story->set_primary_category($primary_cid);
+
+	# remove all keywords if updating
+	$story->delete_keywords([ $story->get_keywords ])
+	    if $update and $story->get_keywords;
+	
+	# add keywords, if we have any
+	if ($sdata->{keywords} and $sdata->{keywords}{keyword}) {
+
+	    # collect keyword objects
+	    my @kws;
+	    foreach (@{$sdata->{keywords}{keyword}}) {
+		my $kw = Bric::Biz::Keyword->lookup({ name => $_ });
+		$kw ||= Bric::Biz::Keyword->new({ name => $_})->save;
+		push @kws, $kw;
+	    }
+
+	    # add keywords to the story
+	    $story->add_keywords(\@kws);
+	}
+
+	# remove all contributors if updating
+	$story->delete_contributors([ $story->get_contributors ])
+	    if $update and $story->get_contributors;
+
+	# add contributors, if any
+	if ($sdata->{contributors} and $sdata->{contributors}{contributor}) {
+	    foreach my $c (@{$sdata->{contributors}{contributor}}) {
+		my %init = (fname => $c->{fname}, 
+			    mname => $c->{mname},
+			    lname => $c->{lname});
+		my ($contrib) = 
+		    Bric::Util::Grp::Parts::Member::Contrib->list(\%init);
+		die __PACKAGE__ . "::create : no contributor found matching " .
+		    "(contributer => " . 
+			join(', ', map { "$_ => $c->{$_}" } keys %$c)
+		    unless defined $contrib;
+		$story->add_contributor($contrib, $c->{role});
+	    }
+	}
+
+	# save the story in an inactive state.  this is necessary to
+	# allow element addition - you can't add elements to an
+	# unsaved story, strangely.
+	$story->deactivate;
+	$story->save;
+
+	# updates are in-place, no need to futz with workflows and desks
+	my $desk;
+	unless ($update) {
+	    # find a suitable workflow and desk for the story.  Might be
+	    # nice if Bric::Biz::Workflow->list took a type key...
+	    foreach my $workflow (Bric::Biz::Workflow->list()) {
+		if ($workflow->get_type == STORY_WORKFLOW) {
+		    $story->set_workflow_id($workflow->get_id());
+		    $desk = $workflow->get_start_desk;
+		    $desk->accept({'asset' => $story});
+		    last;
+		}
+	    }
+	}
+
+	# add element data (no need to delete first on update,
+	# _load_element already has to)
+	if ($sdata->{elements}) {
+	    $pkg->_load_element(element   => $story->get_tile, 
+				data      => $sdata->{elements},
+			        relations => \@relations);
+	} else {
+	    # load an empty set to create a really empty story
+	    $pkg->_load_element(element   => $story->get_tile, 
+				data      => {});
+	}
+
+	# save the story and desk after activating if desired
+	$story->activate if $sdata->{active};
+	$desk->save unless $update;
+	$story->save;
+
+	# checkin and save again for good luck
+	$story->checkin();
+	$story->save();
+
+	# all done, setup the story_id
+	push(@story_ids, $story_ids{$id} = $story->get_id);
+    }
+
+    # resolve relations
+    # FIX: more checking here would be nice
+    foreach my $r (@relations) {
+	if (defined $r->{story_id}) {
+	    if ($r->{relative}) {
+		$r->{container}->set_related_instance_id($story_ids{$r->{story_id}});
+	    } else {
+		$r->{container}->set_related_instance_id($r->{story_id});
+	    }
+	} else {
+	    # FIX: do related media stuff when Bric::SOAP::Media is done
+	}
+	$r->{container}->save;
+    }
+    
+    return name(ids => [ map { name(id => $_) } @story_ids ]);
+}
+
 
 =item $pkg->_load_element(element => $element, data => $sdata->{elements}, relations => \@relations);
 
