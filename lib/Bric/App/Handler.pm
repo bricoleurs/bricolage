26a17bae6e1afcf4dc6728d3873a630999c0224c
David Wheeler <david@justatheory.com>
2002-02-27 00:33:01 +0000

    Loading missing classes. The are Bric::Biz::Asset::Business::Media::Audio and Bric::Biz::Asset::Business::Media::Video. Added even though they don't really do much at this point. But Bricolage complains without them.

--- lib/Bric/App/Handler.pm
+++ lib/Bric/App/Handler.pm
@@ -6,16 +6,16 @@ Bric::App::Handler - The center of the application, as far as Apache is concerne
 
 =head1 VERSION
 
-$Revision: 1.9.2.1 $
+$Revision: 1.9.2.2 $
 
 =cut
 
 # Grab the Version Number.
-our $VERSION = (qw$Revision: 1.9.2.1 $ )[-1];
+our $VERSION = (qw$Revision: 1.9.2.2 $ )[-1];
 
 =head1 DATE
 
-$Date: 2002-01-17 02:12:17 $
+$Date: 2002-02-27 00:33:01 $
 
 =head1 SYNOPSIS
 
@@ -74,7 +74,9 @@ use HTML::Mason::ApacheHandler (args_method => MASON_ARGS_METHOD);
     use Apache::Util qw(escape_html escape_uri);
     use Bric::Config qw(:auth_len :admin :time :dist :ui :prev :ssl);
     use Bric::Biz::Asset::Business::Media;
+    use Bric::Biz::Asset::Business::Media::Audio;
     use Bric::Biz::Asset::Business::Media::Image;
+    use Bric::Biz::Asset::Business::Media::Video;
     use Bric::Biz::Asset::Business::Parts::Tile::Container;
     use Bric::Biz::Asset::Business::Story;
     use Bric::Biz::Asset::Formatting;
