12f682e826970120093d44e22e8074518c1c81df
David Wheeler <david@justatheory.com>
2002-03-09 01:42:48 +0000

    Moved burner.sql into burner.pl. The latter is smart enough to know not to execute the same SQL again.

---
+++ inst/upgrade/1.2.0/burner.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl -w
+
+BEGIN {
+    $ENV{BRICOLAGE_ROOT} ||= '/usr/local/bricolage';
+    eval { require Bric };
+    if ($@) {
+	# We need to set PERL5LIB.
+	require File::Spec::Functions;
+	my $lib =  File::Spec::Functions::catdir($ENV{BRICOLAGE_ROOT}, 'lib');
+	unshift @INC, $lib;
+	$ENV{PERL5LIB} = $lib;
+
+	# Try again.
+	eval { require Bric };
+	die "Cannot locate Bricolage libraries.\n" if $@;
+    }
+};
+
+use Bric::Util::DBI qw(:all);
+
+# Check to see if we've run this before.
+my $sth = prepare('SELECT burner FROM element');
+eval { execute($sth) };
+exit unless $@;
+
+# Okay, we haven't run this before. Let's do it!
+# Start by starting the transaction.
+begin();
+
+# Now update it all.
+eval {
+    $sth = prepare('ALTER TABLE element ADD COLUMN burner NUMERIC(2,0) NOT NULL');
+    execute($sth);
+    $sth = prepare('ALTER TABLE element ALTER burner SET DEFAULT 1');
+    execute($sth);
+    $sth = prepare('UPDATE element SET burner = 1');
+    execute($sth);
+};
+if ($@) {
+    rollback();
+    die "Update failed. Database was not affected. Error: $@;";
+} else {
+    commit();
+}
+
--- inst/upgrade/1.2.0/burner.sql
+++
@@ -1,9 +0,0 @@
-/* These queries update the element table so that all elements will be
- * associated with a particular burner. The default, 1, represents * Mason.
- * Other burners can take other IDs. Currently, 1 is used for * Mason, and 2 is
- * used for HTML::Template. This feature is new to 1.2.0.
-*/
-
-ALTER TABLE element ADD COLUMN burner NUMERIC(2,0) NOT NULL;
-ALTER TABLE element ALTER burner SET DEFAULT 1;
-UPDATE element SET burner = 1;
