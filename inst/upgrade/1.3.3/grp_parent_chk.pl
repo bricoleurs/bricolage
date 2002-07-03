f7e61f5b6e56ea4c4518303ee98916be0d0efbcb
David Wheeler <david@justatheory.com>
2002-07-03 05:14:09 +0000

    Added test for existing circular references. If they exist, the script prints an error message and exits with "1" for an error. Sam, is the proper way to make an upgrade fail, you think?

--- inst/upgrade/1.3.3/grp_parent_chk.pl
+++ inst/upgrade/1.3.3/grp_parent_chk.pl
@@ -11,6 +11,16 @@ exit if fetch_sql(qq{
     WHERE  rcname = 'ck_grp__parent_id_not_eq_id'
 });
 
+if (fetch_sql('select 1 from grp where id = parent_id')) {
+    print "\n\n", '#' x 66, "\n",
+      "# Cannot upgrade because you have circular references in the grp #\n",
+      "# table in the database. Please resolve this conflict and then   #\n",
+      "# try to upgrade again.                                          #\n",
+      '#' x 66, "\n\n";
+    exit 1;
+}
+
+
 do_sql(q{
     ALTER TABLE grp
     ADD CONSTRAINT ck_grp__parent_id_not_eq_id
