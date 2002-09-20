6e1c69bb028f2f2aec787e90bdbf5aeaaa5376e8
David Wheeler <david@justatheory.com>
2002-09-20 18:31:15 +0000

    Improved deactivated asset cleanup. Running this script when you notice multiple assets on a desk, or deativated assets on a desk, should clean them out. This problem should go away once assets are kept on desks by some means other than group memberships.

--- inst/upgrade/1.4.1/bric_shelve.pl
+++ inst/upgrade/1.4.1/bric_shelve.pl
@@ -6,8 +6,27 @@ use lib "$FindBin::Bin/../lib";
 use bric_upgrade qw(:all);
 use Bric::Util::DBI qw(:all);
 
-# This is really simple, and won't hurt anything.
+# This is really simple, and shouldn't hurt anything.
 for (qw(story media formatting)) {
-    do_sql "UPDATE $_ SET workflow__id = NULL where active = 0";
-}
+    do_sql
+      # Set workflow ID to null for deactivated assets.
+      "UPDATE $_ SET workflow__id = NULL where active = 0",
 
+      # Delete desk asset grp membership for deactivated assets.
+      qq{DELETE FROM member
+         WHERE  id IN (
+                  SELECT member__id
+                  FROM   ${_}_member
+                  WHERE  object_id IN (
+                           SELECT id
+                           FROM   $_
+                           WHERE  active = 0
+                         )
+                )
+                AND grp__id IN (
+                  SELECT id
+                  FROM   grp
+                  WHERE  name = 'Desk Assets'
+                )
+      };
+}
