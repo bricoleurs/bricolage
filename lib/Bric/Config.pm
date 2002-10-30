db86afb8ac605e5ccc6b91c2c49eae618870026d
David Wheeler <david@justatheory.com>
2002-10-30 00:24:35 +0000

    It turns out that INIT isn't needed.

--- lib/Bric/Config.pm
+++ lib/Bric/Config.pm
@@ -7,15 +7,15 @@ Bric::Config - A class to hold configuration settings.
 
 =head1 VERSION
 
-$Revision: 1.50.2.3 $
+$Revision: 1.50.2.4 $
 
 =cut
 
-our $VERSION = (qw$Revision: 1.50.2.3 $ )[-1];
+our $VERSION = (qw$Revision: 1.50.2.4 $ )[-1];
 
 =head1 DATE
 
-$Date: 2002-10-29 23:20:30 $
+$Date: 2002-10-30 00:24:35 $
 
 =head1 SYNOPSIS
 
@@ -472,11 +472,9 @@ our %EXPORT_TAGS = (all       => \@EXPORT_OK,
     use constant PROFILE => $config->{PROFILE} || 0;
 
     # Okay, now load the end-user's code, if any.
-    INIT {
-        if ($config->{PERL_LOADER} and $ENV{MOD_PERL}) {
-            my $pkg = TEMPLATE_BURN_PKG;
-            eval "package $pkg; $config->{PERL_LOADER}";
-        }
+    if ($config->{PERL_LOADER} and $ENV{MOD_PERL}) {
+        my $pkg = TEMPLATE_BURN_PKG;
+        eval "package $pkg; $config->{PERL_LOADER}";
     }
 }
 
