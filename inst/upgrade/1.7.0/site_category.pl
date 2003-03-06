e21fe4ed8f244c6482375932d3db61d88fa95369
David Wheeler <david@justatheory.com>
2003-03-06 15:05:39 +0000

    Simplified and tested. Also added index.

--- inst/upgrade/1.7.0/site_category.pl
+++ inst/upgrade/1.7.0/site_category.pl
@@ -5,30 +5,27 @@ use FindBin;
 use lib "$FindBin::Bin/../lib";
 use bric_upgrade qw(:all);
 
-my $add_col = q/ALTER TABLE category ADD site__id NUMERIC(10,0) NOT NULL/;
-my $add_ref = q/ALTER TABLE category ADD
-                CONSTRAINT fk_site__category FOREIGN KEY (site__id)
-                REFERENCES site(id) ON DELETE CASCADE/;
-my $pop_col = q/UPDATE category /.
-              q/SET    site__id = 100 /.
-              q/WHERE  site__id IS NULL/;
+exit if test_sql 'SELECT 1 FROM category WHERE id = 0 AND site__id = 100';
 
-# Add the new column if its not already there.  Not using 'test_sql' here since
-# table alterations seem to be outside of begin() and end() transaction scope.
-eval { do_sql $add_col };
+do_sql
+  # Add the new site__id column.
+  q/ALTER TABLE category ADD site__id NUMERIC(10, 0)/,
 
-# If we have an error, die if its anything but 'Error adding...'
-if ($@) {
-    unless ($@ =~ /"site__id" already exists/) {
-        die "\n\nError adding column 'site__id':\n\n$@\n";
-    }
-}
-# No error, the column was just added, apply foreign key constraints
-else {
-    do_sql $add_ref;
-}
+  # Populate it with data.
+  q/UPDATE category SET site__id = 100/,
 
-# Populate the new site__id field
-do_sql $pop_col;
+  # Add a NOT NULL constraint.
+  q{ALTER TABLE category
+      ADD CONSTRAINT ck_category_null
+      CHECK (site__id IS NOT NULL)},
+
+  # Add a foreign key constraint.
+  q/ALTER TABLE category ADD
+    CONSTRAINT fk_category__site FOREIGN KEY (site__id)
+    REFERENCES site(id) ON DELETE CASCADE/,
+
+  # Add an index.
+  q/CREATE INDEX fkx_category__site ON category(site__id)/
+  ;
 
 __END__
