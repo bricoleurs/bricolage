0da1ada2683d5da85c0621c462da834d2dc3aa80
David Wheeler <david@justatheory.com>
2003-03-07 23:14:25 +0000

    Damn copy-n-paste! Site events now properly relate to site objects, instead of keyword objects.

--- inst/upgrade/1.7.0/site_events.pl
+++ inst/upgrade/1.7.0/site_events.pl
@@ -11,15 +11,15 @@ do_sql
 
   q{INSERT INTO event_type (id, key_name, name, description, class__id, active)
     VALUES (NEXTVAL('seq_event_type'), 'site_new', 'Site Created',
-            'Site was created.', 41, 1)},
+            'Site was created.', 75, 1)},
 
   q{INSERT INTO event_type (id, key_name, name, description, class__id, active)
     VALUES (NEXTVAL('seq_event_type'), 'site_save', 'Site Saved',
-            'Site profile changes were saved.', 41, 1)},
+            'Site profile changes were saved.', 75, 1)},
 
   q{INSERT INTO event_type (id, key_name, name, description, class__id, active)
     VALUES (NEXTVAL('seq_event_type'), 'site_deact', 'Site Deactivated',
-            'Site was deactivated.', 41, 1)}
+            'Site was deactivated.', 75, 1)}
   ;
 
 __END__
