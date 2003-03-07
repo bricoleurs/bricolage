5b96e1ef5dfdd2ade22f43576d04620e7fdfd3bb
Garth Webb <garth.webb@gmail.com>
2003-03-07 18:26:55 +0000

    Updated to convert key_names using perl's y///

--- inst/upgrade/1.7.0/asset_type_data_key_name.pl
+++ inst/upgrade/1.7.0/asset_type_data_key_name.pl
@@ -11,9 +11,24 @@ exit if test_sql "SELECT 1 WHERE EXISTS (SELECT key_name FROM at_data)";
 do_sql
   # Rename the column.
   q/ALTER TABLE at_data RENAME name TO key_name/,
-  q/UPDATE at_data
-    SET    key_name = TRANSLATE(LOWER(key_name), ' ', '_')/
-  ;
+;
+
+update_all();
+
+sub update_all {
+    my $get_key_name = prepare('SELECT key_name FROM at_data');
+    my $set_key_name = prepare('UPDATE at_name SET key_name=? WHERE key_name=?');
+
+    my $name;
+    execute($get_key_name);
+    bind_columns($get_key_name, \$name);
+
+    while (fetch($get_key_name)) {
+        my $key_name = lc($name);
+        $key_name =~ y/a-z/_/cs;
+        execute($set_key_name, $key_name, $name);
+    }
+}
 
 __END__
 
--- inst/upgrade/1.7.0/asset_type_key_name.pl
+++ inst/upgrade/1.7.0/asset_type_key_name.pl
@@ -4,6 +4,7 @@ use strict;
 use FindBin;
 use lib "$FindBin::Bin/../lib";
 use bric_upgrade qw(:all);
+use Bric::Util::DBI qw(:all);
 
 # Just exit if these changes have already been made.
 exit if test_sql "SELECT 1 WHERE EXISTS (SELECT key_name FROM element)";
@@ -12,11 +13,11 @@ exit if test_sql "SELECT 1 WHERE EXISTS (SELECT key_name FROM element)";
 do_sql
   # Add the new column.
   q/ALTER TABLE element ADD key_name VARCHAR(64)/,
+;
 
-  # Populate it with data. I think that this may not be the best approach...
-  # What about non-ascii characters?
-  q/UPDATE element SET key_name = TRANSLATE(LOWER(name), ' ', '_')/,
+update_all();
 
+do_sql
   # Add a NOT NULL constraint.
   q{ALTER TABLE element
       ADD CONSTRAINT ck_key_name__null
@@ -28,5 +29,21 @@ do_sql
   # Drop the old index on the name column.
   qq{DROP INDEX udx_element__name}
   ;
+
+
+sub update_all {
+    my $get_name     = prepare('SELECT name FROM element');
+    my $set_key_name = prepare('UPDATE element SET key_name=? WHERE name=?');
+
+    my $name;
+    execute($get_name);
+    bind_columns($get_name, \$name);
+
+    while (fetch($get_name)) {
+        my $key_name = lc($name);
+        $key_name =~ y/a-z/_/cs;
+        execute($set_key_name, $key_name, $name);
+    }
+}
 
 __END__
