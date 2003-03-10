41ed0b2698dbd85c1a20269a32c39217037ecba1
David Wheeler <david@justatheory.com>
2003-03-10 19:03:32 +0000

    Fixed TZ issues. I was comparing times to times I generated locally. Now the tests properly set up the TZ environment variable from the "Time Zone" preference before running, since it's the time zone in that preference that's always supposed to be returned, not the machine's TZ.

--- t/Bric/Util/Time/Test.pm
+++ t/Bric/Util/Time/Test.pm
@@ -6,6 +6,7 @@ use Test::More;
 use Bric::Util::DBI qw(DB_DATE_FORMAT);
 use Bric::Config qw(:time);
 use Bric::Util::Pref;
+use Bric::Util::Time qw(:all);
 use POSIX ();
 
 my $epoch = 315561600; # gmtime = 315532800;
@@ -15,20 +16,19 @@ my $utc_iso_date = '1980-01-01 08:00:00';
 my $db_date = POSIX::strftime(DB_DATE_FORMAT, gmtime($epoch));
 
 my $pref_format = Bric::Util::Pref->lookup_val('Date/Time Format');
-my $local_date = POSIX::strftime($pref_format, localtime($epoch));
 my $utc_date = POSIX::strftime($pref_format, gmtime($epoch));
+my $local_date;
+{
+    local $ENV{TZ} = Bric::Util::Pref->lookup_val('Time Zone');
+    $local_date = POSIX::strftime($pref_format, localtime($epoch));
+}
 
 my $format = '%m/%d/%Y at %T';
 my $fmt_local = '01/01/1980 at 00:00:00';
 my $fmt_utc = '01/01/1980 at 08:00:00';
 
 ##############################################################################
-# Test class loading.
-##############################################################################
-sub _test_load : Test(1) {
-    use_ok('Bric::Util::Time', ':all');
-}
-
+# Test strfdate().
 sub test_strfdate : Test(4) {
     is( strfdate($epoch), $local_iso_date,
         "Check strfdate is '$local_iso_date'" );
@@ -40,6 +40,8 @@ sub test_strfdate : Test(4) {
         "Check strfdate is '$fmt_utc" );
 }
 
+##############################################################################
+# Test local_date().
 sub test_local_date : Test(5) {
     is( local_date($utc_date), $local_date,
         "Check local date is '$local_date'" );
@@ -47,16 +49,30 @@ sub test_local_date : Test(5) {
         "Check local date is '$fmt_local'" );
     is( local_date($utc_date, 'epoch'), $epoch,
         "Check that local date is '$epoch'" );
-    # Hope that the clock doesn't click over during this test.
-    is( local_date(undef, ISO_8601_FORMAT, 1),
-        POSIX::strftime(ISO_8601_FORMAT, localtime),
+
+    my $current;
+    {
+        # Make sure to use the time zone from the preferences.
+        local $ENV{TZ} = Bric::Util::Pref->lookup_val('Time Zone');
+        $current = POSIX::strftime(ISO_8601_FORMAT, localtime);
+
+    }
+    # Hope that the clock hasn't click over during this test.
+    is( local_date(undef, ISO_8601_FORMAT, 1), $current,
         "Check local date is current" );
-    # Hope that the clock doesn't click over during this test.
-    is( local_date(undef, undef, 1),
-        POSIX::strftime($pref_format, localtime),
+
+    {
+        # Make sure to use the time zone from the preferences.
+        local $ENV{TZ} = Bric::Util::Pref->lookup_val('Time Zone');
+        $current = POSIX::strftime($pref_format, localtime);
+    }
+    # Hope that the clock hasn't click over during this test.
+    is( local_date(undef, undef, 1), $current,
         "Check local date is current" );
 }
 
+##############################################################################
+# Test db_date().
 sub test_db_date : Test(2) {
     is( db_date($local_iso_date), $db_date, "Check db date is '$db_date'" );
     # Hope that the clock doesn't click over during this test.
