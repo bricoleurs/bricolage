faa97f5491893f37bcea465f5af6d99761540b1a
David Wheeler <david@justatheory.com>
2003-03-19 06:41:27 +0000

    Fix merged from HEAD.

--- lib/Bric/Util/DBI.pm
+++ lib/Bric/Util/DBI.pm
@@ -8,18 +8,18 @@ Bric::Util::DBI - The Bricolage Database Layer
 
 =head1 VERSION
 
-$Revision: 1.21.2.4 $
+$Revision: 1.21.2.5 $
 
 =cut
 
 # Grab the Version Number.
-our $VERSION = (qw$Revision: 1.21.2.4 $ )[-1];
+our $VERSION = (qw$Revision: 1.21.2.5 $ )[-1];
 
 =pod
 
 =head1 DATE
 
-$Date: 2003-03-19 03:28:43 $
+$Date: 2003-03-19 06:41:27 $
 
 =head1 SYNOPSIS
 
@@ -731,10 +731,11 @@ sub build_query_with_unions {
     my (@queries);
     # loop through the relations building a query
     foreach my $rel ( @{$pkg->RELATIONS} ) {
-        my $rtables = $pkg->RELATION_TABLES->{$rel};
+        my $rtables = "," . $pkg->RELATION_TABLES->{$rel};
         my $rwhere_clause = $pkg->RELATION_JOINS->{$rel};
+        $rtables = "" if length($rtables) == 1;
         push @queries, qq{ SELECT DISTINCT $cols
-                           FROM   $tables, $rtables
+                           FROM   $tables $rtables
                            WHERE  $where_clause AND $rwhere_clause
                          };
     }
