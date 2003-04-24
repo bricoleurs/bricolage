c010c4ed8c44729e98f9904eb3455916e7252b36
David Wheeler <david@justatheory.com>
2003-04-24 14:40:36 +0000

    Asset desk upgrade fix. The inst/upgrade/1.5.1/asset_desk.pl script, which is run by make upgrade, now correctly checks to see if the upgrade has already been performed.

--- inst/upgrade/1.5.1/asset_desk.pl
+++ inst/upgrade/1.5.1/asset_desk.pl
@@ -6,23 +6,23 @@ use FindBin;
 use lib "$FindBin::Bin/../lib";
 use bric_upgrade qw(:all);
 
-foreach (qw(story formatting media)) {
-    next if fetch_sql(q{SELECT 1 FROM $_ WHERE desk__id IS NULL OR desk__id IS NOT NULL});
+foreach my $table (qw(story formatting media)) {
+    next if test_sql(qq{SELECT 1 WHERE EXISTS (SELECT desk__id FROM $table)});
     do_sql
-      qq{ALTER TABLE $_ ADD COLUMN desk__id NUMERIC(10,0)},
+      qq{ALTER TABLE $table ADD COLUMN desk__id NUMERIC(10,0)},
       qq{
-        UPDATE $_
+        UPDATE $table
         SET desk__id = to_number(asv.short_val, 9999999999)
-        FROM    attr_${_}_val asv, attr_$_ a
+        FROM    attr_${_}_val asv, attr_$table a
         WHERE   asv.id = (
                           SELECT max(id)
                           FROM   attr_${_}_val asv
-                          WHERE  asv.object__id = $_.id
+                          WHERE  asv.object__id = $table.id
                        )
         AND     a.id = asv.attr__id
         AND     a.subsys = 'deskstamps'
-        AND     $_.workflow__id IS NOT NULL
+        AND     $table.workflow__id IS NOT NULL
       },
-      qq{CREATE INDEX fdx_${_}__desk__id ON $_(desk__id)}
+      qq{CREATE INDEX fdx_${_}__desk__id ON $table(desk__id)}
 }
 
