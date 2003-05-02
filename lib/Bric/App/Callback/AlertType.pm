6ccab33d7affc7bd6169eca302f0f91c0c654ad3
Scott Lanning <slanning@cpan.org>
2003-05-02 08:47:48 +0000

    alert_type callback

--- lib/Bric/App/Callback/AlertType.pm
+++ lib/Bric/App/Callback/AlertType.pm
@@ -3,6 +3,33 @@ package Bric::App::Callback::AlertType;
 use base qw(Bric::App::Callback);
 __PACKAGE__->register_subclass(class_key => 'alert_type');
 use strict;
+use Bric::App::Authz;
+use Bric::App::Event;
 use Bric::App::Session;
+use Bric::App::Util;
+use Bric::Util::Language;
+use Bric::Util::Priv::Parts::Const qw(EDIT);
+
+my $disp_name = get_disp_name(CLASS_KEY);
+my $class = get_package_name(CLASS_KEY);
+
+sub delete : Callback {
+    my $self = shift;
+    my ($param, $field) = @{ $self->request_args }['param', 'field'];
+
+    foreach my $id (@{ mk_aref($param->{$field}) }) {
+        my $at = $class->lookup({ id => $id }) || next;
+        if (chk_authz($at, EDIT, 1)) {
+            $at->remove();
+            $at->save();
+            log_event(CLASS_KEY . '_del', $at);
+        } else {
+            my $lang = Bric::Util::Language->get_handle(LANGUAGE);
+            my $name = '&quot;' . $at->get_name() . '&quot';
+            add_msg($lang->maketext("Permission to delete [_1] denied.", $name));
+        }
+    }
+}
+
 
 1;
