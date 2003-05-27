924d6ea85c8948fd10bc409547f052af6a035681
Scott Lanning <slanning@cpan.org>
2003-05-27 14:27:54 +0000

    this callback was empty and not called as far as I can tell

--- lib/Bric/App/Callback/FileUpload.pm
+++
@@ -1,8 +0,0 @@
-package Bric::App::Callback::FileUpload;
-
-use base qw(Bric::App::Callback);
-__PACKAGE__->register_subclass(class_key => 'file_upload');
-use strict;
-use Bric::App::Session;
-
-1;
