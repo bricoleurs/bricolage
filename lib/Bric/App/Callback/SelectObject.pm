82a438785458734bd07301dbcb2a8058ccde8fea
Scott Lanning <slanning@cpan.org>
2003-05-28 14:18:22 +0000

    cb subversion

--- lib/Bric/App/Callback/SelectObject.pm
+++ lib/Bric/App/Callback/SelectObject.pm
@@ -3,5 +3,39 @@ package Bric::App::Callback::SelectObject;
 use base qw(Bric::App::Callback);
 __PACKAGE__->register_subclass(class_key => 'select_object');
 use strict;
+use Bric::App::Session qw(:state);
+use HTML::Mason::Commands;
+
+
+# XXX: look at it later...
+my ($object) = $field =~ /\|([^-]+)-/;
+my $sub_widget = "$widget.$object";
+
+if ($field eq "$widget|$object-selected_id_cb") {
+    # Handle auto-repopulation of this form.
+    my $name = get_state_data($sub_widget, 'form_name');
+
+    set_state_data($sub_widget, 'selected_id', $param->{$name});
+}
+
+
+sub clear : Callback {
+    my $self = shift;
+    my $trigger = $self->param_field;
+
+    # If the trigger field was submitted with a true value, clear state!
+    if ($self->param->{$trigger}) {
+        my $s = \%HTML::Mason::Commands::session;
+        
+        # Find all the select_object widget information
+        my @sel = grep(substr($_,0,13) eq 'select_object', keys %$s);
+        
+        # Clear out all the state data.
+        foreach my $sub_widget (@sel) {
+            set_state_data($sub_widget, {});   # XXX: $sub_widget
+        }
+    }
+}
+
 
 1;
