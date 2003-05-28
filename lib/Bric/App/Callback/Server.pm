3024700e57853fa0f52d78179e6e7a6f18fa0ae7
Scott Lanning <slanning@cpan.org>
2003-05-28 15:22:33 +0000

    server cb done

--- lib/Bric/App/Callback/Server.pm
+++ lib/Bric/App/Callback/Server.pm
@@ -3,5 +3,29 @@ package Bric::App::Callback::Server;
 use base qw(Bric::App::Callback);
 __PACKAGE__->register_subclass(class_key => 'server');
 use strict;
+use Bric::App::Authz qw(:all);
+use Bric::App::Event qw(log_event);
+use Bric::App::Util qw(:all);
+
+
+my $type = 'server';
+my $disp_name = get_disp_name($type);
+my $class = get_package_name($type);
+my $dest_class = get_package_name('dest');
+
+
+sub delete : Callback {
+    my $self = shift;
+
+    my $dest = $dest_class->lookup({ id => $self->param->{dest_id} });
+    chk_authz($dest, EDIT);
+    foreach my $id (@{ mk_aref($self->param_field) }) {
+        my $s = $class->lookup({ id => $id }) || next;
+        $s->del;
+        $s->save;
+        log_event('server_del', $s);
+    }
+}
+
 
 1;
