968dc974445146b4ee4e288469baf7964a2f3814
Scott Lanning <slanning@cpan.org>
2003-05-28 15:43:55 +0000

    site cb done

--- lib/Bric/App/Callback/Site.pm
+++ lib/Bric/App/Callback/Site.pm
@@ -3,5 +3,39 @@ package Bric::App::Callback::Site;
 use base qw(Bric::App::Callback);
 __PACKAGE__->register_subclass(class_key => 'site');
 use strict;
+use Bric::App::Authz qw(:all);
+use Bric::App::Event qw(log_event);
+use Bric::App::Util qw(:all);
+
+my $type = 'site';
+my $disp_name = get_disp_name($type);
+my $class = get_package_name($type);
+my $c = Bric::App::Cache->new();    # singleton
+
+
+sub delete : Callback {
+    my $self = shift;
+
+    my $flag;
+    foreach my $id (@{ mk_aref($self->param_field) }) {
+        my $site = $class->lookup({ id => $id }) || next;
+        if (chk_authz($site, EDIT, 1)) {
+            $site->deactivate;
+            $site->save;
+            $c->set_lmu_time;
+            log_event("${type}_deact", $site);
+            $flag = 1;
+        } else {
+            my $name = '&quot;' . $site->get_name . '&quot';
+            my $msg = "Permission to delete [_1] denied.";
+            add_msg($self->lang->maketext($msg, $name));
+        }
+    }
+    if ($flag) {
+        $c->set('__SITES__', 0);
+        $c->set('__WORK_FLOWS__', 0);
+    }
+}
+
 
 1;
