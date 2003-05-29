0ab90e4a65817cef5dc777d07ef2905d334dcdd0
Scott Lanning <slanning@cpan.org>
2003-05-29 10:18:07 +0000

    workflow cb done

--- lib/Bric/App/Callback/Workflow.pm
+++ lib/Bric/App/Callback/Workflow.pm
@@ -3,29 +3,36 @@ package Bric::App::Callback::Workflow;
 use base qw(Bric::App::Callback);
 __PACKAGE__->register_subclass(class_key => 'workflow');
 use strict;
+use Bric::App::Authz qw(:all);
+use Bric::App::Event qw(log_event);
+use Bric::App::Util qw(:all);
 
 my $type = 'workflow';
 my $disp_name = get_disp_name($type);
 my $class = get_package_name($type);
 
 
-return unless $field eq "$widget|delete_cb";
-my $flag;
-foreach my $id (@{ mk_aref($param->{$field}) }) {
-    my $wf = $class->lookup({ id => $id }) || next;
-    if (chk_authz($wf, EDIT, 1)) {
-	$wf->deactivate;
-	$wf->save;
-	log_event("${type}_deact", $wf);
-	$flag = 1;
-    } else {
-	my $name = '&quot;' . $wf->get_name . '&quot';
-        add_msg($lang->maketext("Permission to delete [_1] denied.",$name));
+sub delete : Callback {
+    my $self = shift;
+
+    my $flag = 0;
+    foreach my $id (@{ mk_aref($self->value) }) {
+        my $wf = $class->lookup({ id => $id }) || next;
+        if (chk_authz($wf, EDIT, 1)) {
+            $wf->deactivate;
+            $wf->save;
+            log_event("${type}_deact", $wf);
+            $flag = 1;
+        } else {
+            my $msg = "Permission to delete [_1] denied.";
+            my $name = '&quot;' . $wf->get_name . '&quot';
+            add_msg($self->lang->maketext($msg, $name));
+        }
     }
-}
-if ($flag) {
-    $c->set('__SITES__', 0);
-    $c->set('__WORK_FLOWS__', 0);
+    if ($flag) {
+        $self->cache->set('__SITES__', 0);
+        $self->cache->set('__WORK_FLOWS__', 0);
+    }
 }
 
 
