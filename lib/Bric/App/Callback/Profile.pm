f0e3b83341fba93e980591b578e3c973fc75a689
Scott Lanning <slanning@cpan.org>
2003-06-03 17:16:37 +0000

    Does this look right?
    I have the class Bric::App::Callback::Profile as the
    base profile class which inherits from Bric::App::Callback.
    Its constructor overrides the default and basically does
    some things common to all the profiles, and also it sets
    an accessor has_perms which says whether the user has
    permissions (this is also part of the "common" stuff).
    Then the idea is each subclass in Profile will inherit
    this constructor, and all of their Callback methods
    will first return unless $self->has_perms. So this
    basically replaces how it used to be where any callback
    called with "profile|something_cb" had the common code
    execute before "forking off" $m->comp("$key.mc") which
    was one of 18 mason components.

--- lib/Bric/App/Callback/Profile.pm
+++ lib/Bric/App/Callback/Profile.pm
@@ -1,52 +1,96 @@
 package Bric::App::Callback::Profile;
 
 use base qw(Bric::App::Callback);
-__PACKAGE__->register_subclass(class_key => 'profile');
+__PACKAGE__->register_subclass('class_key' => 'profile');
+use HTML::Mason::MethodMaker('read_write' => [qw(obj type class has_perms)]);
 use strict;
 use Bric::App::Authz qw(:all);
 use Bric::App::Callback::Util qw(parse_uri);
 use Bric::App::Util qw(:all);
 
+use Bric::App::Callback::Profile::Action.pm;
+use Bric::App::Callback::Profile::AlertType.pm;
+use Bric::App::Callback::Profile::Category.pm;
+use Bric::App::Callback::Profile::Contrib.pm;
+use Bric::App::Callback::Profile::Desk.pm;
+use Bric::App::Callback::Profile::Dest.pm;
+use Bric::App::Callback::Profile::ElementData.pm;
+use Bric::App::Callback::Profile::ElementType.pm;
+use Bric::App::Callback::Profile::FormBuilder.pm;
+use Bric::App::Callback::Profile::Grp.pm;
+use Bric::App::Callback::Profile::Job.pm;
+use Bric::App::Callback::Profile::Media.pm;
+use Bric::App::Callback::Profile::MediaType.pm;
+use Bric::App::Callback::Profile::OutputChannel.pm;
+use Bric::App::Callback::Profile::Pref.pm;
+use Bric::App::Callback::Profile::Server.pm;
+use Bric::App::Callback::Profile::Site.pm;
+use Bric::App::Callback::Profile::Source.pm;
+use Bric::App::Callback::Profile::Story.pm;
+use Bric::App::Callback::Profile::Template.pm;
+use Bric::App::Callback::Profile::User.pm;
+use Bric::App::Callback::Profile::Workflow.pm;
 
-my $excl = { desk => 1, action => 1, server => 1 };
+my $excl = {'desk' => 1, 'action' => 1, 'server' => 1};
 
 
-sub SOMENAMES : Callback {
-    my $self = shift;
+# each subclass of Profile inherits this constructor
+# which I want to set up the common things (obj, type, class)
+# and also whether the user has permission then each callback
+# will just return unless $self->has_perms
+sub new {
+    my $pkg = shift;
+    my $self = bless($pkg->SUPER::new(@_), $pkg);
 
-    my $type = (parse_uri($self->apache_req->uri))[2];
+    my $param = $self->request_args;
 
-    # Get the class name.
-    my $key = $type eq 'contrib' && ! defined $param->{contrib_id}
-      ? 'person'
-      : $type;
-
-    my $class = get_package_name($key);
-    $class = $param->{grp_type} || $class if $type eq 'grp';
+    $self->type((parse_uri($self->apache_req->uri))[2]);
+    $self->class($get_class->($param, $self->type));
 
     # Instantiate the object.
-    my $id = defined $param->{$type . '_id'} && $param->{$type . '_id'} ne ''
-      ? $param->{$type . '_id'} : undef;
-    my $obj = defined $id ? $class->lookup({ id => $id }) : $class->new;
+    my $id = defined $param->{$self->type . '_id'} && $param->{$self->type . '_id'} ne ''
+      ? $param->{$self->type . '_id'} : undef;
+    my $obj = defined $id ? $self->class->lookup({ id => $id }) : $self->class->new;
+    $self->obj($obj);
 
     # Check the permissions.
-    unless ( $excl->{$type} || chk_authz($obj, (defined $id ? EDIT : CREATE), 1)
-               || ($type eq 'user' && $obj->get_id == get_user_id()) ) {
+    unless ($excl->{$self->type}
+              || chk_authz($self->obj, (defined $id ? EDIT : CREATE), 1)
+              || ($self->type eq 'user' && $self->obj->get_id == get_user_id()))
+    {
         # If we're in here, the user doesn't have permission to do what
         # s/he's trying to do.
         add_msg("Changes not saved: permission denied.");
         set_redirect(last_page());
-        return;
+        $self->has_perms(0);
+    } else {
+        $self->has_perms(1);
     }
 
-    # Process its data
+    # Process its data - in a subclass:
     # action, alert_type, category, contrib, desk, dest, element_data,
     # element_type, grp, job, media_type, output_channel, pref,
     # server, site, source, user, workflow
-    $param->{obj} = $m->comp("$type.mc", %ARGS, obj => $obj, class => $class);
 }
 
 
+###
+
+my $get_class = sub {
+    my ($param, $type) = @_;
+
+    my $key = $type;
+    if ($type eq 'contrib' && ! defined $param->{'contrib_id'}) {
+        $key = 'person';
+    }
+
+    my $class = get_package_name($key);
+    if ($type eq 'grp' && defined $param->{'grp_type'}) {
+        $class = $param->{'grp_type'};
+    }
+
+    return $class;
+};
 
 
 1;
