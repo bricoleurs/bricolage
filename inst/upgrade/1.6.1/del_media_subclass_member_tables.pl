591e2afc858703c7e4d47bdb5b402dceaeb8aee8
David Wheeler <david@justatheory.com>
2003-06-04 00:25:55 +0000

    Fixe for multiple runs. Not it won't generate an error if it's run twice.

--- inst/upgrade/1.6.1/del_media_subclass_member_tables.pl
+++ inst/upgrade/1.6.1/del_media_subclass_member_tables.pl
@@ -5,6 +5,8 @@ use FindBin;
 use lib "$FindBin::Bin/../lib";
 use bric_upgrade qw(:all);
 
+exit if test_sql "SELECT 1 WHERE EXISTS (SELECT id FROM image_member)";
+
 # We should never have used the image, audio, and video member tables.
 foreach my $key (qw(image audio video)) {
     do_sql
