d7183f7e453c2ffbed476eb9eecf81c22cdc97c6
Scott Lanning <slanning@cpan.org>
2003-06-09 15:26:01 +0000

    dist cb renamed dist 'profile' cbs to 'dist' moved delete cb into Profile

--- lib/Bric/App/Callback/Profile/Dest.pm
+++ lib/Bric/App/Callback/Profile/Dest.pm
@@ -3,6 +3,7 @@ package Bric::App::Callback::Profile::Dest;
 use base qw(Bric::App::Callback::Package);
 __PACKAGE__->register_subclass('class_key' => 'dest');
 use strict;
+use Bric::App::Authz qw(:all);
 use Bric::App::Event qw(log_event);
 use Bric::App::Util qw(:all);
 use Bric::Biz::OutputChannel;
@@ -99,6 +100,27 @@ sub save : Callback {
         } else {
             # It's a new destination. Let them add Actions and Servers.
             return $dest;
+        }
+    }
+}
+
+
+# strictly speaking, this is a Manager (not a Profile) callback
+
+sub delete : Callback {
+    my $self = shift;
+
+    foreach my $id (@{ mk_aref($self->value) }) {
+        my $dest = $class->lookup({ id => $id }) || next;
+        if (chk_authz($dest, EDIT, 1)) {
+            $dest->del_output_channels;
+            $dest->deactivate;
+            $dest->save;
+            log_event("${type}_deact", $dest);
+        } else {
+            my $name = '&quot;' . $dest->get_name . '&quot';
+            my $msg = "Permission to delete [_1] denied.";
+            add_msg($self->lang->maketext($msg, $name));
         }
     }
 }
