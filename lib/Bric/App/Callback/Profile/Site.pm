b52aef2800da557bd933563f29111f350b1f59a5
Scott Lanning <slanning@cpan.org>
2003-06-10 10:24:54 +0000

    site cb rename 'profile' to CLASS_KEY move delete cb to Profile/Site

--- lib/Bric/App/Callback/Profile/Site.pm
+++ lib/Bric/App/Callback/Profile/Site.pm
@@ -3,6 +3,7 @@ package Bric::App::Callback::Profile::Site;
 use base qw(Bric::App::Callback::Package);
 __PACKAGE__->register_subclass('class_key' => 'site');
 use strict;
+use Bric::App::Authz qw(:all);
 use Bric::App::Event qw(log_event);
 use Bric::App::Util qw(:all);
 use Bric::Util::Fault qw(rethrow_exception isa_bric_exception);
@@ -10,6 +11,7 @@ use Bric::Util::Grp;
 
 my $type = CLASS_KEY;
 my $disp_name = get_disp_name($type);
+my $class = get_package_name($type);
 
 
 sub save : Callback {
@@ -79,6 +81,34 @@ sub save : Callback {
     rethrow_exception($err) unless isa_bric_exception($err, 'Error');
     add_msg($self->lang->maketext($err->maketext));
     return $site;
+}
+
+
+# strictly speaking, this is a Manager (not a Profile) callback
+
+sub delete : Callback {
+    my $self = shift;
+    my $c = $self->cache;
+
+    my $flag;
+    foreach my $id (@{ mk_aref($self->value) }) {
+        my $site = $class->lookup({'id' => $id}) || next;
+        if (chk_authz($site, EDIT, 1)) {
+            $site->deactivate();
+            $site->save();
+            $c->set_lmu_time();
+            log_event("${type}_deact", $site);
+            $flag = 1;
+        } else {
+            my $arg = '&quot;' . $site->get_name . '&quot';
+            my $msg = "Permission to delete [_1] denied.";
+            add_msg($self->lang->maketext($msg, $arg));
+        }
+    }
+    if ($flag) {
+        $c->set('__SITES__', 0);
+        $c->set('__WORK_FLOWS__', 0);
+    }
 }
 
 
