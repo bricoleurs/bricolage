fc0eeb07f850f3481d1f39e9d01292152d604cff
Scott Lanning <slanning@cpan.org>
2003-06-10 10:46:50 +0000

    used Bric::App::Session->instance cleaned-up

--- lib/Bric/App/Callback/SelectTime.pm
+++ lib/Bric/App/Callback/SelectTime.pm
@@ -1,15 +1,10 @@
 package Bric::App::Callback::SelectTime;
 
-# XXX: I think this needs looked at closer because there
-# are some conventions on passing "subwidget" data
-# that might not make the transition to MasonX::...
-
 use base qw(Bric::App::Callback);
-__PACKAGE__->register_subclass(class_key => 'select_time');
+__PACKAGE__->register_subclass('class_key' => 'select_time');
 use strict;
 use Bric::App::Session qw(:state);
 use Bric::App::Util qw(:all);
-
 
 my $defs = {
     min  => '00',
@@ -31,7 +26,7 @@ sub refresh : Callback(priority => 0) {
         my $sub_widget = CLASS_KEY . ".$b";
         my @vals;
         my $incomplete = 0;
-        # This is compliments $incomplete.  It tells whether *any* time fields
+        # This is complements $incomplete.  It tells whether *any* time fields
         # were set.  This way we know if they started to add time fields, but
         # then stopped.
         my $has_data = 0;
@@ -63,7 +58,7 @@ sub refresh : Callback(priority => 0) {
                 $param->{$b} = undef;
             }
         } else {
-            my $date = sprintf("%04d-%02d-%02d %02d:%02d:00", @vals);
+            my $date = sprintf('%04d-%02d-%02d %02d:%02d:00', @vals);
             # Write the date to the parameters.
             $param->{$b} = $date;
         }
@@ -75,7 +70,7 @@ sub clear : Callback {
 
     # If the trigger field was submitted with a true value then, clear state!
     if ($is_clear_state->($self->request_args)) {
-        my $s = \%HTML::Mason::Commands::session;
+        my $s = Bric::App::Session->instance;
 
         # Find all the select_time widget information
         my @sel = grep(substr($_,0,11) eq 'select_time', keys %$s);
