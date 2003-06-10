8489010b563a9eb707d8ca451b4f8827c84c1515
Scott Lanning <slanning@cpan.org>
2003-06-10 11:07:47 +0000

    select_object cb subversion suppressed

--- lib/Bric/App/Callback/SelectObject.pm
+++ lib/Bric/App/Callback/SelectObject.pm
@@ -1,24 +1,22 @@
 package Bric::App::Callback::SelectObject;
 
 use base qw(Bric::App::Callback);
-__PACKAGE__->register_subclass(class_key => 'select_object');
+__PACKAGE__->register_subclass('class_key' => 'select_object');
 use strict;
 use Bric::App::Session qw(:state);
-use HTML::Mason::Commands;
 
 
-# XXX: look at it later...
-my ($object) = $field =~ /\|([^-]+)-/;
-my $sub_widget = "$widget.$object";
+sub save_selected_id : Callback {
+    my $self = shift;
 
-if ($field eq "$widget|$object-selected_id_cb") {
+    my $object = $self->value;
+    my $sub_widget = CLASS_KEY . '.' . $object;
+
     # Handle auto-repopulation of this form.
     my $name = get_state_data($sub_widget, 'form_name');
-
     set_state_data($sub_widget, 'selected_id', $param->{$name})
       unless ref $param->{$name};
 }
-
 
 sub clear : Callback {
     my $self = shift;
@@ -26,14 +24,14 @@ sub clear : Callback {
 
     # If the trigger field was submitted with a true value, clear state!
     if ($self->request_args->{$trigger}) {
-        my $s = \%HTML::Mason::Commands::session;
-        
+        my $s = Bric::App::Session->instance;
+
         # Find all the select_object widget information
         my @sel = grep(substr($_,0,13) eq 'select_object', keys %$s);
         
         # Clear out all the state data.
         foreach my $sub_widget (@sel) {
-            set_state_data($sub_widget, {});   # XXX: $sub_widget
+            set_state_data($sub_widget, {});
         }
     }
 }
