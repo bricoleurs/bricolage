18d3ab7823da13dbd65a2cbe79bbbe754049ecac
David Wheeler <david@justatheory.com>
2003-06-12 18:28:30 +0000

    Uncommented calls to status_msg().

--- lib/Bric/App/Callback/Publish.pm
+++ lib/Bric/App/Callback/Publish.pm
@@ -42,7 +42,7 @@ sub preview : Callback {
 
         # Move out the story and then redirect to preview.
         my $url = $b->preview($media, 'media', get_user_id(), $oc_id);
-#        status_msg("Redirecting to preview.");
+        status_msg("Redirecting to preview.");
         redirect_onload($url);
     } else {
         my $s = get_state_data('story_prof', 'story');
@@ -68,7 +68,7 @@ sub preview : Callback {
         }
         # Move out the story and then redirect to preview.
         my $url = $b->preview($s, 'story', get_user_id(), $oc_id);
-#        status_msg("Redirecting to preview.");
+        status_msg("Redirecting to preview.");
         redirect_onload($url);
     }
 }
