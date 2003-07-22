6a5980ed71542c765661023e7c122af419162acc
Scott Lanning <slanning@cpan.org>
2003-07-22 14:10:48 +0000

    sync with Email Action that David added to HEAD (Callback/Profile/Action.pm only, so it will be broken until merged with HEAD)

--- lib/Bric/App/Callback/Profile/Action.pm
+++ lib/Bric/App/Callback/Profile/Action.pm
@@ -22,40 +22,50 @@ sub save : Callback {
     my $param = $self->request_args;
     my $act = $self->obj;
 
-    if (!defined $param->{action_id}) {
-        # This is a new action. Set the type and return.
-        $act->set_type($param->{type});
-        $act->set_server_type_id($param->{dest_id});
-        $act->set_ord($param->{ord});
-        $act->save;
-        if ($act->has_more) {
-            $param->{'obj'} = $act;
+    my $ret = eval {
+        if (!defined $param->{action_id}) {
+            # This is a new action. Set the type and return.
+            $act->set_type($param->{type});
+            $act->set_server_type_id($param->{dest_id});
+            $act->set_ord($param->{ord});
+
+            unless ($param->{save_it} or not $act->has_more) {
+                $param->{'obj'} = $act;
+                return;
+            }
+        }
+
+        # Set the redirection.
+        my $name = '&quot;' . $act->get_name . '&quot;';
+
+        if ($param->{delete}) {
+            # Delete it.
+            $act->del;
+            $act->save;
+            log_event('action_del', $act);
+            add_msg("$disp_name profile $name deleted.");
+            set_redirect("/admin/profile/dest?id=$param->{dest_id}");
             return;
         }
-    }
 
-    # Set the redirection.
-    set_redirect("/admin/profile/dest?id=$param->{dest_id}");
-    my $name = '&quot;' . $act->get_name . '&quot;';
-
-    if ($param->{delete}) {
-        # Delete it.
-        $act->del;
+        # Roll in the changes. Assume it's active.
+        foreach my $meth ($act->my_meths(1)) {
+            next if $meth->{name} eq 'type';
+            $meth->{set_meth}->($act, @{$meth->{set_args}}, $param->{$meth->{name}})
+              if defined $meth->{set_meth};
+        }
         $act->save;
-        log_event('action_del', $act);
-        add_msg("$disp_name profile $name deleted.");
-        return;
-    }
+        log_event('action_' . (defined $param->{action_id} ? 'save' : 'new'), $act);
+        add_msg("$disp_name profile $name saved.");
+        set_redirect("/admin/profile/dest?id=$param->{dest_id}");
+    };
 
-    # Roll in the changes. Assume it's active.
-    foreach my $meth ($act->my_meths(1)) {
-        next if $meth->{name} eq 'type';
-        $meth->{set_meth}->($act, @{$meth->{set_args}}, $param->{$meth->{name}})
-          if defined $meth->{set_meth};
-    }
-    $act->save;
-    log_event('action_' . (defined $param->{action_id} ? 'save' : 'new'), $act);
-    add_msg("$disp_name profile $name saved.");
+    my $err = $@ or return $ret;
+    # Catch Error exceptions and turn them into error messages.
+    die $err unless isa_bric_exception($err, 'Error');
+    add_msg($self->lang->maketext($err->maketext));
+    $param->{'obj'} = $act;
+    return;
 }
 
 
