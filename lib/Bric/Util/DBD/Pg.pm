0e750bb5d79f0077205a4f3198a6cb97ed20170a
David Wheeler <david@justatheory.com>
2003-08-01 05:03:24 +0000

    Reformatted documentation.

--- lib/Bric/Util/DBD/Pg.pm
+++ lib/Bric/Util/DBD/Pg.pm
@@ -1,122 +1,93 @@
 package Bric::Util::DBD::Pg;
 
-=pod
-
 =head1 NAME
 
-Bric::Util::DBD::Pg - The Bricolage PostgreSQL Driver
+Bric::Util::DBD::Pg - Bricolage PostgreSQL database adaptor
 
-=head1 VERSION
+=head1 VITALS
 
-$Revision: 1.9 $
+=over 4
+
+=item Version
+
+$Revision: 1.10 $
 
 =cut
 
 # Grab the Version Number.
-our $VERSION = (qw$Revision: 1.9 $ )[-1];
+our $VERSION = (qw$Revision: 1.10 $ )[-1];
 
-=pod
+=item Date
 
-=head1 DATE
+$Date: 2003-08-01 05:03:24 $
 
-$Date: 2003-08-01 04:39:51 $
+=item CVS ID
+
+$Id: Pg.pm,v 1.10 2003-08-01 05:03:24 wheeler Exp $
+
+=back
 
 =head1 SYNOPSIS
 
-In Bric::Util::DBI(1), just
+In F<bricolage.conf>:
 
-  use Bric::Util::DBD::Pg ':all';
+  DBD_TYPE = Pg
 
 =head1 DESCRIPTION
 
-This module exports into Bric::Util::DBIs name space a number of
-database-dependent functions and variables such that Bric::Util::DBI can interface
-seamlessly with a PostgreSQL database. See Bric::Util::DBI for details.
-Bric::Util::DBD::Pg should never be C<use>d in any other Bricolage class. Nor should any
-other Bric::DBD::* driver.
-
-=head1 INTERFACE
-
-NONE.
-
-=head2 Constructors
-
-NONE.
-
-=head2 Destructors
-
-NONE.
-
-=head2 Public Class Methods
-
-NONE.
-
-=head2 Public Instance Methods
-
-NONE.
-
-=head1 PRIVATE
-
-NONE.
-
-=head2 Private Class Methods
-
-NONE.
-
-=head2 Private Instance Methods
-
-NONE.
-
-=head2 Private Functions
-
-NONE.
+This module exports into Bric::Util::DBI's namespace a number of
+database-dependent functions and variables such that Bric::Util::DBI can
+interface seamlessly with a PostgreSQL database. See Bric::Util::DBI for
+details. Bric::Util::DBD::Pg should never be C<use>d in any other Bricolage
+class. Nor should any other Bric::DBD::* driver.
 
 =cut
 
-################################################################################
+##############################################################################
 # Dependencies
-################################################################################
+##############################################################################
 # Standard Dependencies
 use strict;
 
-################################################################################
+##############################################################################
 # Programmatic Dependences
 use DBD::Pg;
 use Bric::Config qw(:dbi);
 use Bric::Util::Fault qw(throw_dp)
 
-################################################################################
+##############################################################################
 # Constants
-################################################################################
-
-# This variable tells Bric::Util::DBI whether this driver supports transactions
+##############################################################################
+# This variable tells Bric::Util::DBI whether this driver supports
+# transactions.
 use constant TRANSACTIONAL => 1;
+
+# This is the DSN that Bric::Util::DBI will use to connect to the database.
 use constant DSN_STRING => 'dbname=' . DB_NAME
   . (DB_HOST ? eval "';host=' . DB_HOST" : '')
   . (DB_PORT ? eval "';port=' . DB_PORT" : '');
 
-################################################################################
+##############################################################################
 # Inheritance
-################################################################################
+##############################################################################
 use base qw(Exporter);
 our @EXPORT_OK = qw(last_key_sql next_key_sql db_date_parts DSN_STRING
 		    TRANSACTIONAL);
 our %EXPORT_TAGS = (all => \@EXPORT_OK);
 
-################################################################################
+##############################################################################
 # Private Functions
-################################################################################
+##############################################################################
 BEGIN {
      $ENV{PGTZ} = 'UTC';
-#    $ENV{PGTZ} = Bric::Config::DB_TIME_ZONE; # The default timezone from Config
 #    $ENV{PGDATESTYLE} = 'ISO';          # Should default to this, anyway.
 #    $ENV{PGCLIENTENCODING} = 'UNICODE'; # Should default to this, anyway.
 #    $ENV{PGSERVERENCODING} = 'UNICODE'; # Should default to this, anyway.
 } # BEGIN
 
-################################################################################
+##############################################################################
 # Exportable Functions
-################################################################################
+##############################################################################
 sub last_key_sql {
     # Returns the SQL to fetch the last key fetched by this process for a table.
     # Used by Bric::Util::DBI::last_key().
@@ -125,7 +96,7 @@ sub last_key_sql {
     return "SELECT CURRVAL('${db_name}seq_$table_name')";
 } # last_key_sql()
 
-################################################################################
+##############################################################################
 sub next_key_sql {
     # Returns the SQL to fetch the next key for a table. Intended to be used
     # inside an INSERT query, not on its own. Used by Bric::Util::DBI::next_key().
@@ -134,7 +105,7 @@ sub next_key_sql {
     return $db_name . "NEXTVAL('${db_name}seq_$table_name')";
 } # next_key()
 
-################################################################################
+##############################################################################
 sub db_date_parts {
     # This function unpacks a date/time string as it is formatted for the
     # database and returns a list of time tokens for use by timelocal(). Used by
@@ -149,16 +120,23 @@ sub db_date_parts {
 1;
 __END__
 
-=head1 NOTES
-
-NONE.
-
 =head1 AUTHOR
 
-David Wheeler <david@wheeler.net>
+David Wheeler <david@kineticode.com>
 
 =head1 SEE ALSO
 
-L<Bric::Util::DBI|Bric::Util::DBI>
+=over 4
+
+=item L<Bric::Util::DBI|Bric::Util::DBI>
+
+The Bricolage DBI interface.
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2001 About.com. See L<Bric::License|Bric::License> for complete
+license terms and conditions.
 
 =cut
