b8d5e0599b6eb0518961b98f861580186f43a9fc
David Wheeler <david@justatheory.com>
2004-03-04 15:59:33 +0000

    Updates from Florian. Includes new license.

--- contrib/button_gen/bricolage_workflow_buttons.pl
+++ contrib/button_gen/bricolage_workflow_buttons.pl
@@ -1,16 +1,35 @@
 #!/usr/bin/perl
 
 #
-# bric_workflow_buttons.pl
-# (c) 2004 Florian Rossol (rossol@yola.in-berlin.de)
+# Copyright (c) 2004, Florian Rossol (rossol@yola.in-berlin.de)
+# All rights reserved.
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the license under which Bricolage is distributed.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# Bricolage License for more details.
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+#  * Neither the authors name nor the names of the contributors may be
+#    used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
 use File::Path;
@@ -83,9 +102,11 @@ sub bric_buttons {
 	
 	# save the image
 	if ($gif == 1) {
+	    gimp_convert_indexed($img, 0, 2, 0, 0, 0, 0);
 	    file_gif_save(RUN_NONINTERACTIVE, $img, $activelayer,
 			  "$out_dir/$name", "$out_dir/$name", 
 			  0, 0, 0, 0);
+	    gimp_convert_rgb($img);
 	} else {
 	    file_png_save(RUN_NONINTERACTIVE, $img, $activelayer,
 			  "$out_dir/$name",
--- contrib/button_gen/bricolage_workspace_buttons.pl
+++ contrib/button_gen/bricolage_workspace_buttons.pl
@@ -1,16 +1,35 @@
 #!/usr/bin/perl
 
 #
-# bric_workspace_buttons.pl
-# (c) 2004 Florian Rossol (rossol@yola.in-berlin.de)
+# Copyright (c) 2004, Florian Rossol (rossol@yola.in-berlin.de)
+# All rights reserved.
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the license under which Bricolage is distributed.
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# Bricolage License for more details.
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+#  * Neither the authors name nor the names of the contributors may be
+#    used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
 use File::Path;
@@ -20,8 +39,8 @@ use Gimp::Fu;
 sub bric_buttons { 
     #Gimp::set_trace(TRACE_ALL);
    
-    my($text, $font, $bgcolor, $shadow, $fgcolor, $webbgcolor, 
-       $out_dir, $gif) = @_;
+    my($text, $font, $bgcolor_i, $shadow_i, $bgcolor_ii, $shadow_ii, 
+       $star_color, $fgcolor, $webbgcolor, $out_dir, $gif) = @_;
     
     chomp($text);
 
@@ -33,53 +52,85 @@ sub bric_buttons {
     # Create a new image
     my $img = gimp_image_new(150, 20, RGB);
 
-    print "generating my_workspace_on.";
-    if ($gif == 1) {
-	print ".gif\n";
-    } else {
-	print ".png\n";
-    }
-    
-    # Create a new layer for the background and add it to the image
-    my $background = gimp_layer_new($img, 150, 20, RGB, "Background", 100,
-					    NORMAL_MODE);
-    gimp_image_add_layer($img, $background, 1);
-	    
-    # Fill the background layer
-    gimp_palette_set_background($webbgcolor);
-    gimp_edit_fill($background, BG_IMAGE_FILL);
-    
-    # select the shadow and fill it
-    gimp_rect_select($img, 1, 1, 149, 19, 2, 0, 0);
-    gimp_palette_set_background($shadow);
-    gimp_edit_fill($background, BG_IMAGE_FILL);
+    foreach (1, 2) {
 
-    # select the background and fill it
-    gimp_rect_select($img, 0, 0, 148, 18, 2, 0, 0);
-    gimp_palette_set_background($bgcolor);
-    gimp_edit_fill($background, BG_IMAGE_FILL);
+	my $name = "my_workspace_";
+	if ($_ == 1) {
+	    $name .= "on";
+	} elsif ($_ == 2) {
+	    $name .= "off";
+	}
+	if ($gif == 1) {
+	    $name .= ".gif";
+	} else {
+	    $name .= ".png";
+	}
+	print "generating $name\n";
+	
+	# Create a new layer for the background and add it to the image
+	my $background = gimp_layer_new($img, 150, 20, RGB, "Background", 100,
+					NORMAL_MODE);
+	gimp_image_add_layer($img, $background, 1);
 
-    # Choose color of text
-    gimp_palette_set_foreground($fgcolor);
+	# Fill the background layer
+	gimp_palette_set_background($webbgcolor);
+	gimp_edit_fill($background, BG_IMAGE_FILL);
+	
+	if ($_ == 1) {
+            # select the shadow and fill it
+	    gimp_rect_select($img, 1, 1, 149, 19, 2, 0, 0);
+	    gimp_palette_set_background($shadow_i);
+	    gimp_edit_fill($background, BG_IMAGE_FILL);
 	    
-    # Create the text layer. Using -1 as the drawable creates a new layer. 
-    $text_layer = gimp_text_fontname($img, -1, 20, 6, $text,
+	    # select the background and fill it
+	    gimp_rect_select($img, 0, 0, 148, 18, 2, 0, 0);
+	    gimp_palette_set_background($bgcolor_i);
+	    gimp_edit_fill($background, BG_IMAGE_FILL);
+	} elsif ($_ == 2) {
+	    gimp_palette_set_background($bgcolor_ii);
+	    gimp_edit_fill($background, BG_IMAGE_FILL);
+
+	    gimp_rect_select($img, 1, 18, 150, 20, 2, 0, 0);
+	    gimp_rect_select($img, 149, 1, 150, 20, 0, 0, 0);
+	    gimp_palette_set_background($shadow_ii);
+	    gimp_edit_fill($background, BG_IMAGE_FILL);
+
+	    # make the star
+	    gimp_free_select($img, 10, 
+			     [10, 3, 12, 6, 16, 7, 13, 10, 14, 13, 10, 12, 6,
+			          13, 7, 10, 4, 7, 8, 6],
+			     2, 1, 0, 0);
+	    gimp_palette_set_background($star_color);
+	    gimp_edit_fill($background, BG_IMAGE_FILL);
+	    
+	}
+	# Choose color of text
+	gimp_palette_set_foreground($fgcolor);
+	
+	# Create the text layer. Using -1 as the drawable creates a new layer. 
+	$text_layer = gimp_text_fontname($img, -1, 20, 6, $text,
 					 0, 1, 10, 0, $font);
-    
-    # merge visible layers
-    my ($activelayer) = gimp_image_merge_visible_layers($img, 0);
-    
-    # save the image
-    if ($gif == 1) {
-	file_gif_save(RUN_NONINTERACTIVE, $img, $activelayer,
-		      "$out_dir/$_/$name.gif", "$out_dir/$_/$name.gif", 
-		      0, 0, 0, 0);
-    } else {
-	file_png_save(RUN_NONINTERACTIVE, $img, $activelayer,
-		      "$out_dir/my_workspace_on.png", "$out_dir/my_workspace_on.png", 
-		      0, 9, 0, 0, 0, 0, 0);
+	
+	# merge visible layers
+	my ($activelayer) = gimp_image_merge_visible_layers($img, 0);
+	
+	# save the image
+	if ($gif == 1) {
+	    gimp_convert_indexed($img, 0, 0, 100, 0, 0, 0);
+	    file_gif_save(RUN_NONINTERACTIVE, $img, $activelayer,
+			  "$out_dir/$name", "$out_dir/$name", 
+			  0, 0, 0, 0);
+	    gimp_convert_rgb($img);
+	} else {
+	    file_png_save(RUN_NONINTERACTIVE, $img, $activelayer,
+			  "$out_dir/$name", "$out_dir/$name", 
+			  0, 9, 0, 0, 0, 0, 0);
+	}
+
+	# remove the layer from $img
+	gimp_image_remove_layer($img, $activelayer);
+
     }
-    
     return;
 }
 	
@@ -99,8 +150,11 @@ register
      [PF_STRING, "text", "Text", "MY WORKSPACE"],
      [PF_FONT, "font", "Schrift",	
 	"-microsoft-verdana-bold-r-normal-*-10-*-*-*-*-*-*-*"],
-     [PF_COLOR,	"backgroundcolor", "Background Color", "#d7840f"],
-     [PF_COLOR,	"shadow", "Shadow", "#938654"],
+     [PF_COLOR,	"backgroundcolor_i", "Background Color I", "#d7840f"],
+     [PF_COLOR,	"shadow_i", "Shadow I", "#938654"],
+      [PF_COLOR, "backgroundcolor_ii", "Background Color II", "#cccc99"],
+     [PF_COLOR,	"shadow_ii", "Shadow II", "#999966"],
+     [PF_COLOR,	"star_color", "Color of the star", "#cc9a03"],
      [PF_COLOR,	"textcolor", "Text Color", "#000000"],
      [PF_COLOR, "website_backgroundcolor", "Background Color of Website",
       "#FFFFFF"],
