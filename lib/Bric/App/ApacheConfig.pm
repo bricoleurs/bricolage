25a7da2c746a82992b113617cd2b1b39d76c8f94
David Wheeler <david@justatheory.com>
2004-03-16 06:56:09 +0000

    Fix for Location directive load order. This prevented Mason from working properly in the local preview server for some setups. Most likely, there were problems with subdirectories of /media, too, and we never noticed. This fixes the problem by instituting a more manual process of dynamically configuring Apache. It's all one big string, now, and we get to control the order of things. Coincidentally, this should also eliminate the problem with <Perl> directives from mod_perl 1.26 and earlier.

--- lib/Bric/App/ApacheConfig.pm
+++ lib/Bric/App/ApacheConfig.pm
@@ -48,232 +48,216 @@ directory to @INC by using Makefile.PL. Just a thought.
 use strict;
 use Bric::App::ApacheStartup;
 use Bric::Config qw(:char);
-our $DEBUGGING;
+use constant DEBUGGING => 0;
 
-my $silent_config = ! MANUAL_APACHE && $mod_perl::VERSION > 1.26;
-my %VirtualHost_lcl;
-my @NameVirtualHost_lcl = ([ NAME_VHOST . ':' . LISTEN_PORT ]);
 do {
+    my @names = ('NameVirtualHost ' . NAME_VHOST . ':' . LISTEN_PORT);
     # Set up the basic configuration.
-    my %config = ( DocumentRoot       => MASON_COMP_ROOT->[0][1],
-		   ServerName         => VHOST_SERVER_NAME,
-		   DefaultType        => '"text/html; charset=' . lc CHAR_SET . '"',
-                   AddDefaultCharset  => lc CHAR_SET,
-		   SetHandler         => 'perl-script',
-		   PerlHandler        => 'Bric::App::Handler',
-		   PerlAccessHandler  => 'Bric::App::AccessHandler',
-		   PerlCleanupHandler => 'Bric::App::CleanupHandler',
-		   RedirectMatch      =>
-		     'permanent .*\/favicon\.ico$ /media/images/favicon.ico',
+    my @config = (
+        'DocumentRoot        ' . MASON_COMP_ROOT->[0][1],
+        'ServerName          ' . VHOST_SERVER_NAME,
+        'DefaultType         "text/html; charset=' . lc CHAR_SET . '"',
+        'AddDefaultCharset   ' . lc CHAR_SET,
+        'SetHandler          perl-script',
+        'PerlHandler         Bric::App::Handler',
+        'PerlAccessHandler   Bric::App::AccessHandler',
+        'PerlCleanupHandler  Bric::App::CleanupHandler',
+        'RedirectMatch       '.
+          'permanent .*\/favicon\.ico$ /media/images/favicon.ico',
+    );
 
-		   # setup Apache::DB handler if debugging
-		   ($DEBUGGING ?
-		    (PerlFixupHandler => 'Apache::DB') : ()),
-		 );
+    # Setup Apache::DB handler if debugging
+    push @config, 'PerlFixupHandler    Apache::DB' if DEBUGGING;
 
     # see Apache::SizeLimit manpage
-    $config{PerlFixupHandler} = 'Apache::SizeLimit'
-	if CHECK_PROCESS_SIZE;
+    push @config, 'PerlFixupHandler    Apache::SizeLimit'
+      if CHECK_PROCESS_SIZE;
 
     # This will slow down every request; thus we recommend that previews
     # not be local.
-    $config{PerlTransHandler} = 'Bric::App::PreviewHandler::uri_handler'
-	if PREVIEW_LOCAL;
+    push @config,
+      'PerlTransHandler    Bric::App::PreviewHandler::uri_handler'
+      if PREVIEW_LOCAL;
 
     # This URI will handle logging users out.
-    my %locs = ("/logout"  => {
-        PerlAccessHandler  => 'Bric::App::AccessHandler::logout_handler',
-        PerlCleanupHandler => 'Bric::App::CleanupHandler',
-    });
+    my @locs = (<<"    EOF");
+      <Location /logout>
+        PerlAccessHandler  Bric::App::AccessHandler::logout_handler
+        PerlCleanupHandler Bric::App::CleanupHandler
+      </Location>
+    EOF
+
+    # Mask off Apache::DB handler if debugging - the debugger
+    # seems to cause problems for login for some reason.  With the
+    # Apache::DB handler in place the output from the first screen
+    # after login goes to the debugger's STDOUT instead of the
+    # browser!
+    my $fix = DEBUGGING
+      ? "\n        PerlFixupHandler Apache::OK"
+      : '';
 
     # This URI will handle logging users in.
-    $locs{"/login"} = {
-        SetHandler         => 'perl-script',
-        PerlAccessHandler  => 'Bric::App::AccessHandler::okay',
-        PerlHandler        => 'Bric::App::Handler',
-        PerlCleanupHandler => 'Bric::App::CleanupHandler',
+    push @locs, <<"    EOF";
+      <Location /login>
+        SetHandler         perl-script
+        PerlAccessHandler  Bric::App::AccessHandler::okay
+        PerlHandler        Bric::App::Handler
+        PerlCleanupHandler Bric::App::CleanupHandler$fix
+      </Location>
+    EOF
 
-	# mask off Apache::DB handler if debugging - the debugger
-	# seems to cause problems for login for some reason.  With the
-	# Apache::DB handler in place the output from the first screen
-	# after login goes to the debugger's STDOUT instead of the
-	# browser!
-       ($DEBUGGING ?
-        (PerlFixupHandler  => 'Apache::OK') : ()),
-    };
+    # We might need this for SSL configuration.
+    my $loginref = \$locs[-1];
 
     # This URI will handle all non-Mason stuff that we server (graphics, etc.).
-    $locs{"/media"} = {
-        SetHandler         => 'default-handler',
-        PerlAccessHandler  => 'Apache::OK',
-        PerlCleanupHandler => 'Apache::OK',
+    push @locs, <<"    EOF";
+      <Location /media>
+        SetHandler         default-handler
+        PerlAccessHandler  Apache::OK
+        PerlCleanupHandler Apache::OK$fix
+      </Location>
+    EOF
 
-	# mask off Apache::DB handler if debugging
-       ($DEBUGGING ?
-        (PerlFixupHandler  => 'Apache::OK') : ()),
-    };
-    $locs{"/media/js"} = {
-         ForceType => '"application/x-javascript; charset=' . lc CHAR_SET . '"'
-    };
+    # Force JavaScript to the proper MIME type and always use Unicode.
+    push @locs, <<"    EOF";
+      <Location /media/js>
+        ForceType          "application/x-javascript; charset=utf-8"
+      </Location>
+    EOF
 
     # This will serve media assets and previews.
-    $locs{"/data"} = { SetHandler => 'default-handler' };
+    push @locs, <<"    EOF";
+      <Location /data>
+        SetHandler         default-handler
+      </Location>
+    EOF
 
     # This will run the SOAP server.
-    $locs{'/soap'} = {
-        SetHandler         => 'perl-script',
-        PerlHandler        => 'Bric::SOAP::Handler',
-        PerlAccessHandler  => 'Apache::OK'
-    };
+    push @locs, <<"    EOF";
+      <Location /soap>
+        SetHandler         perl-script
+        PerlHandler        Bric::SOAP::Handler
+        PerlAccessHandler  Apache::OK
+      </Location>
+    EOF
 
     if (ENABLE_DIST) {
-	# This URI will run the distribution server.
-	$locs{"/dist"} = {
-            SetHandler  => 'perl-script',
-            PerlHandler => 'Bric::Dist::Handler'
-        };
+        push @locs, <<"        EOF";
+          <Location /dist>
+            SetHandler         perl-script
+            PerlHandler        Bric::Dist::Handler
+          </Location>
+        EOF
     }
 
     if (QA_MODE) {
-	# Turn on Perl warnings and run Apache::Status.
-	$config{PerlWarn} = 'On';
-	$locs{"/perl-status"} = {
-            SetHandler         => 'perl-script',
-            PerlHandler        => 'Apache::Status',
-            PerlAccessHandler  => 'Apache::OK',
-            PerlCleanupHandler => 'Apache::OK',
-
-            # Mask off Apache::DB handler if debugging
-	   ($DEBUGGING ?  
-            (PerlFixupHandler  => 'Apache::OK') : ()),
-        };
+        # Turn on Perl warnings and run Apache::Status.
+        push @config, 'PerlWarn            On';
+        push @locs, <<"        EOF";
+          <Location /dist>
+            SetHandler         perl-script
+            PerlHandler        Apache::Status
+            PerlAccessHandler  Apache::OK
+            PerlCleanupHandler Apache::OK$fix
+          </Location>
+        EOF
     }
 
     if (PREVIEW_LOCAL) {
-	my $prev_loc = "/" . join('/', PREVIEW_LOCAL);
-	if (PREVIEW_MASON) {
-	    # We need to take some special steps to ensure that Mason properly
-	    # handles the request.
-	    $locs{$prev_loc} = {
-                SetHandler       => 'perl-script',
-                PerlFixupHandler => 'Bric::App::PreviewHandler::fixup_handler',
-                PerlHandler      => 'Bric::App::Handler'
-	    };
-	} else {
-	    # This will ensure that the documents are not cached by the browser, so
-	    # that the preview will always serve the most recently burned file.
-	    $locs{$prev_loc} =
-	      { PerlFixupHandler =>
-		'"sub { $_[0]->no_cache(1); return Apache::OK; }"' };
-	}
+        my $prev_loc = "/" . join('/', PREVIEW_LOCAL);
+        if (PREVIEW_MASON) {
+            # We need to take some special steps to ensure that Mason properly
+            # handles the request.
+            push @locs, <<"            EOF";
+              <Location $prev_loc>
+                SetHandler       perl-script
+                PerlFixupHandler Bric::App::PreviewHandler::fixup_handler
+                PerlHandler      Bric::App::Handler
+              </Location>
+            EOF
+        } else {
+            # This will ensure that the documents are not cached by the browser, so
+            # that the preview will always serve the most recently burned file.
+            push @locs, <<"            EOF";
+              <Location $prev_loc>
+                PerlFixupHandler "sub { \$_[0]->no_cache(1); return Apache::OK; }"
+              </Location>
+            EOF
+        }
     }
-    $config{Location} = \%locs;
-    $VirtualHost_lcl{NAME_VHOST . ':' . LISTEN_PORT} = \%config;
+
+    my @sections = (
+        "<VirtualHost "
+          . NAME_VHOST . ':' . LISTEN_PORT . ">",
+        @config, @locs,
+        '</VirtualHost>'
+    );
 
     if (SSL_ENABLE) {
-	push @NameVirtualHost_lcl, [ NAME_VHOST . ':' . SSL_PORT ];
-	my %ssl_config = (%config,
-		SSLCertificateFile	=> &SSL_CERTIFICATE_FILE,
-		SSLCertificateKeyFile	=> &SSL_CERTIFICATE_KEY_FILE);
+        push @names, 'NameVirtualHost ' . NAME_VHOST . ':' . SSL_PORT;
+        push @config,
+            'SSLCertificateFile     ' . SSL_CERTIFICATE_FILE,
+            'SSLCertificateKeyFile  ' . SSL_CERTIFICATE_KEY_FILE;
 
-	my %ssl_locs = %locs;
-	$ssl_locs{"/login"} = {
-            SetHandler         => 'perl-script',
-            PerlAccessHandler  => 'Bric::App::AccessHandler::okay',
-            PerlHandler        => 'Bric::App::Handler',
-            PerlCleanupHandler => 'Bric::App::CleanupHandler',
-        };
+        # Replace the login location.
+        $loginref = <<"        EOF";
+          <Location /login>
+            SetHandler         perl-script
+            PerlAccessHandler  Bric::App::AccessHandler::okay
+            PerlHandler        Bric::App::Handler
+            PerlCleanupHandler Bric::App::CleanupHandler
+          </Location>
+        EOF
 
-	$ssl_config{Location} = \%ssl_locs;
+        # Apache::ReadConfig does not handle <IfModule>
+        if (MANUAL_APACHE) {
+            if (SSL_ENABLE eq 'apache_ssl') {
+                push @config,
+                  'SSLEnable',
+                  'SSLRequireSSL',
+                  'SSLVerifyClient   0',
+                  'SSLVerifyDepth    10';
+            } else {
+                # is mod_ssl
+                push @config, 'SSLEngine      On';
+            }
+        } else {
+            push @config, <<"            EOF";
+              <IfModule mod_ssl.c>
+                SSLEngine On
+              </IfModule>
+              <IfModule apache_ssl.c>
+                SSLEnable
+                SSLVerifyClient 0,
+                SSLVerifyDepth  10,
+                SSLRequireSSL
+              </IfModule>
+            EOF
+        }
 
-	if ($silent_config) {		# Apache::ReadConfig does not handle <IfModule>
-	    if (SSL_ENABLE eq 'apache_ssl') {
-		$ssl_config{SSLEnable}		= '';
-		$ssl_config{SSLRequireSSL}	= '';
-		$ssl_config{SSLVerifyClient}	= 0;
-		$ssl_config{SSLVerifyDepth}	= 10;
-	    } else {	# is mod_ssl
-		$ssl_config{SSLEngine}		= 'on';
-	    }
-	} else {
-	    my %mod_ssl = ( SSLEngine => 'on' );
-	    $ssl_config{'IfModule mod_ssl.c'} = \%mod_ssl;
-	    my %apache_ssl = (
-		SSLEnable	=> '',
-		SSLVerifyClient	=> 0,
-		SSLVerifyDepth	=> 10,
-		SSLRequireSSL	=> ''
-	    );
-	    $ssl_config{'IfModule apache_ssl.c'} = \%apache_ssl;
-	}
-	$VirtualHost_lcl{NAME_VHOST . ':' . SSL_PORT} = \%ssl_config;
-      }
+        push @sections, "<VirtualHost " . NAME_VHOST . ':' . SSL_PORT . ">",
+          @config, @locs,
+      '</VirtualHost>';
+    }
 
-    if ($silent_config) {
-	# place VirtualHost stuff in Apache's scope
-	package Apache::ReadConfig;
-	our @NameVirtualHost = @NameVirtualHost_lcl;
-	our %VirtualHost = %VirtualHost_lcl;
+    if (MANUAL_APACHE) {
+        # Write out a configuration file and include it.
+        use Bric::Util::Trans::FS;
+        my $conffile = Bric::Util::Trans::FS->cat_dir(TEMP_DIR, 'bricolage',
+                                                      'bric_httpd.conf');
+        open CONF, ">$conffile" or die "Cannot open $conffile for output: $!\n";
+        print CONF $_, $/ for (@names, @sections);
+        close CONF;
+
+        # Place Include directive in Apache's scope
+        package Apache::ReadConfig;
+        our $Include = $conffile;
     } else {
-	# If we get here, then <Perl> sections are broken. See the discussion
-	# here: http://mathforum.org/epigone/modperl/rorphaltwin. As a quick
-	# and dirty fix, let's dump the config to a temp file and just use the
-	# include directive.
-	use Bric::Util::Trans::FS;
-	my $conffile = Bric::Util::Trans::FS->cat_dir(TEMP_DIR, 'bricolage',
-						      'bric_httpd.conf');
-	open CONF, ">$conffile" or die "Cannot open $conffile for output: $!\n";
-	select CONF;
-
-	# Output the NameVirtualHost_lcl directives.
-	print "NameVirtualHost $_->[0]\n" for @NameVirtualHost_lcl;
-	@NameVirtualHost_lcl = ();
-
-	# Output the rest.
-	while (my ($k, $v) = each %VirtualHost_lcl) {
-	    print "<VirtualHost $k>\n";
-	    Bric::App::ApacheConfig::print_bric_directive(undef, $v, 2);
-	    print "</VirtualHost>\n";
-	}
-	close CONF;
-	select STDOUT;
-	%VirtualHost_lcl = ();
-	# place Include directive in Apache's scope
-	package Apache::ReadConfig;
-	our $Include = $conffile;
+        # place VirtualHost stuff in Apache's scope
+        package Apache::ReadConfig;
+        our $PerlConfig = join "\n", @names, @sections;
     }
 };
-
-# This function is required for outputting a configuration file from mod_perl
-# version 1.26 and earlier.
-sub print_bric_directive {
-    my ($directive, $value, $i) = @_;
-    my $indent = ' ' x $i;
-    if ($directive) {
-	if ($directive eq 'Location') {
-	    while (my ($k, $v) = each %$value) {
-		print "$indent<Location $k>\n";
-		print_bric_directive(undef, $v, $i + 2);
-		print "$indent</Location>\n";
-	    }
-	} else {
-	    if (ref $value) {
-		print "$indent<$directive>\n";
-		print_bric_directive(undef, $value, $i + 2);
-		$directive = 'IfModule' if $directive =~ /^IfModule/i;
-		$directive = 'IfDevine' if $directive =~ /^IfDefine/i;
-		print "$indent</$directive>\n";
-	    }
-	    else {
-		print "$indent$directive $value\n";
-	    }
-	}
-    } else {
-	while (my ($k, $v) = each %$value) {
-	    print_bric_directive($k, $v, $i + 2);
-	}
-    }
-}
 
 1;
 
