63aede5db573aac48c9a23c8018345612661a9a0
David Wheeler <david@justatheory.com>
2004-09-18 20:16:22 +0000

    OS-neutral testing.

--- t/Bric/Util/Language/Test.pm
+++ t/Bric/Util/Language/Test.pm
@@ -3,14 +3,14 @@ use strict;
 use warnings;
 use base qw(Bric::Test::Base);
 use Test::More;
+use File::Spec;
 
 ##############################################################################
 # Test class loading.
 ##############################################################################
 sub _test_load : Test(no_plan) {
     use_ok('Bric::Util::Language');
-    # XXX Do we need t make this OS-sensitive?
-    (my $dir = $INC{'Bric/Util/Language.pm'}) =~ s/\.pm$//;
+    my $dir = File::Spec->catdir(qw(lib Bric Util Language), '');
     opendir LANGS, $dir or die "Cannot open directory '$dir': $!\n";
     while (my $lang = readdir LANGS) {
         next unless $lang =~ s/\.pm$//;
