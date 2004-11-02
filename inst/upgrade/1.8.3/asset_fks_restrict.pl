33d3be35817c3474ca49c3d45934b334073be0c2
David Wheeler <david@justatheory.com>
2004-11-02 19:04:54 +0000

    Make sure FKs exist before dropping them.

--- inst/upgrade/1.8.3/asset_fks_restrict.pl
+++ inst/upgrade/1.8.3/asset_fks_restrict.pl
@@ -6,19 +6,21 @@ use FindBin;
 use lib catdir $FindBin::Bin, updir, 'lib';
 use bric_upgrade qw(:all);
 
-#exit if test_foreign_key 'story', 'fk_source__story', 'r';
+exit if test_foreign_key 'story', 'fk_source__story', 'r';
 
 for my $table (qw(story media)) {
     for my $attr (qw(source usr element workflow site desk)) {
+        do_sql "ALTER TABLE $table DROP CONSTRAINT fk_$attr\__$table"
+          if test_foreign_key $table, "fk_$attr\__$table";
         do_sql
-          "ALTER TABLE $table DROP CONSTRAINT fk_$attr\__$table",
           "ALTER TABLE $table
            ADD CONSTRAINT fk_$attr\__$table FOREIGN KEY ($attr\__id)
            REFERENCES $attr(id) ON DELETE RESTRICT";
     }
 
+    do_sql "ALTER TABLE $table DROP CONSTRAINT fk_$table\__$table\_id"
+          if test_foreign_key $table, "fk_$table\__$table\_id";
     do_sql
-      "ALTER TABLE $table DROP CONSTRAINT fk_$table\__$table\_id",
       "ALTER TABLE $table
        ADD CONSTRAINT fk_$table\__$table\_id FOREIGN KEY (alias_id)
        REFERENCES $table(id) ON DELETE RESTRICT";
@@ -26,8 +28,9 @@ for my $table (qw(story media)) {
 
 for my $attr (qw(usr output_channel element category workflow site desk)) {
     my $fkt = $attr eq 'output_channel' ? 'fromatting' : 'formatting';
+    do_sql "ALTER TABLE formatting DROP CONSTRAINT fk_$attr\__formatting"
+          if test_foreign_key 'formatting', "fk_$attr\__formatting";
     do_sql
-      "ALTER TABLE formatting DROP CONSTRAINT fk_$attr\__$fkt",
       "ALTER TABLE formatting
        ADD CONSTRAINT fk_$attr\__formatting FOREIGN KEY ($attr\__id)
        REFERENCES $attr(id) ON DELETE RESTRICT";
