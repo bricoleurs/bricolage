d85ff1b47273b858514dc05a05e49ca09d74d4f7
Scott Lanning <slanning@cpan.org>
2004-12-15 18:15:06 +0000

    fixes the upgrade scripts in 1.8.3 which were failing due to a primary key NOT NULL constraint - this makes it not possible for there to be NOT NULL ids  (if I remember right - it was a few weeks ago)

--- inst/upgrade/1.8.3/story_uri_pk.pl
+++ inst/upgrade/1.8.3/story_uri_pk.pl
@@ -25,6 +25,9 @@ do_sql
   qq{update story_uri  set id = NEXTVAL('seq_story_uri')},
 
   # Make sure it can never be null.
+  qq{ALTER TABLE story_uri
+     ALTER COLUMN id SET NOT NULL},
+
   qq{ALTER TABLE    story_uri
      ADD CONSTRAINT ck_story_uri__id__null CHECK (id IS NOT NULL)},
 
