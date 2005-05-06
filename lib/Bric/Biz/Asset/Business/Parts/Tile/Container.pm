369626f03cfdb2c79064cc1c9621014d2fbf4d5e
David Wheeler <david@justatheory.com>
2005-05-06 17:42:31 +0000

    Always return undef for scalar methods.

--- lib/Bric/Biz/Asset/Business/Parts/Tile/Container.pm
+++ lib/Bric/Biz/Asset/Business/Parts/Tile/Container.pm
@@ -624,7 +624,7 @@ sub get_related_story {
                                          '_related_instance_obj');
 
     # Return with nothing if there is no related instance ID
-    return unless $rel_id;
+    return undef unless $rel_id;
 
     # Clear the object cache if the ID has changed.
     $rel_obj = undef if $rel_obj and ($rel_obj->get_id != $rel_id);
@@ -687,7 +687,7 @@ sub get_related_media {
     my ($media_id, $media_obj) = $self->_get('related_media_id',
                                              '_related_media_obj');
 
-    return unless $media_id;
+    return undef unless $media_id;
 
     # Clear the object cache if the IDs change.
     $media_obj = undef if $media_obj and $media_obj->get_id != $media_id;
