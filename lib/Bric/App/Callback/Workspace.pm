106ee4c555d3c20fdad8738c34e19370fac3e0c9
Marshall Roch <marshall@exclupen.com>
2005-07-30 03:56:54 +0000

    Kill tabs

--- lib/Bric/App/Callback/Workspace.pm
+++ lib/Bric/App/Callback/Workspace.pm
@@ -30,14 +30,14 @@ sub checkin : Callback {
     # Checking in assets and moving them to another desk.
     my %desks;
     foreach my $next (@{ mk_aref($self->params->{"desk_asset|next_desk"})}) {
-	next unless $next;
-	my ($aid, $from_id, $to_id, $key) = split /-/, $next;
-	throw_dp($key);
-	my $a = $pkgs->{$key}->lookup({ id => $aid, checkout => 1 });
-	my $curr = $desks{$from_id} ||= $dskpkg->lookup({ id => $from_id });
-	my $next = $desks{$to_id} ||= $dskpkg->lookup({ id => $to_id });
-	$curr->checkin($a);
-	log_event("${key}_checkin", $a, { Version => $a->get_version });
+        next unless $next;
+        my ($aid, $from_id, $to_id, $key) = split /-/, $next;
+        throw_dp($key);
+        my $a = $pkgs->{$key}->lookup({ id => $aid, checkout => 1 });
+        my $curr = $desks{$from_id} ||= $dskpkg->lookup({ id => $from_id });
+        my $next = $desks{$to_id} ||= $dskpkg->lookup({ id => $to_id });
+        $curr->checkin($a);
+        log_event("${key}_checkin", $a, { Version => $a->get_version });
 
         if ($curr->get_id != $next->get_id) {
             $curr->transfer({ to    => $next,
@@ -56,27 +56,27 @@ sub delete : Callback {
     # Deleting assets.
     foreach my $key (@$keys) {
         foreach my $aid (@{ mk_aref($self->params->{"${key}_delete_ids"}) }) {
-	    my $a = $pkgs->{$key}->lookup({ id => $aid, checkout => 1 });
-	    if (chk_authz($a, EDIT, 1)) {
-		my $d = $a->get_current_desk;
-		$d->checkin($a);
-		$d->remove_asset($a);
-		$d->save;
-		log_event("${key}_rem_workflow", $a);
+            my $a = $pkgs->{$key}->lookup({ id => $aid, checkout => 1 });
+            if (chk_authz($a, EDIT, 1)) {
+                my $d = $a->get_current_desk;
+                $d->checkin($a);
+                $d->remove_asset($a);
+                $d->save;
+                log_event("${key}_rem_workflow", $a);
                 $a->set_workflow_id(undef);
-		$a->deactivate;
-		$a->save;
+                $a->deactivate;
+                $a->save;
 
                 if($key eq 'formatting') {
                     $burn->undeploy($a);
                     my $sb = Bric::Util::Burner->new({user_id => get_user_id()});
                     $sb->undeploy($a);
                 }
-		log_event("${key}_deact", $a);
-	    } else {
+                log_event("${key}_deact", $a);
+            } else {
                 add_msg('Permission to delete "[_1]" denied.', $a->get_name);
-	    }
-	}
+            }
+        }
     }
 }
 
