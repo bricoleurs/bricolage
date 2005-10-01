e5dc221fc2afbcf669fd8162cba34eeafda9ad16
David Wheeler <david@justatheory.com>
2005-10-01 03:09:25 +0000

    Rename SOAP tests.

---
+++ t/Bric/SOAP/ATType/Test.pm
@@ -0,0 +1,15 @@
+package Bric::SOAP::ATType::Test;
+use strict;
+use warnings;
+use base qw(Bric::Test::Base);
+use Test::More;
+
+##############################################################################
+# Test class loading.
+##############################################################################
+sub _test_load : Test(1) {
+    use_ok('Bric::SOAP::ATType');
+}
+
+1;
+__END__
