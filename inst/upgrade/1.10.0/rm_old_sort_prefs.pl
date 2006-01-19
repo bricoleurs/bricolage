cb9eae8b37c85a24af21fa8fe279d55c9497ba58
David Wheeler <david@justatheory.com>
2006-01-19 20:53:44 +0000

    Upgrade script fixes.

--- inst/upgrade/1.10.0/rm_old_sort_prefs.pl
+++ inst/upgrade/1.10.0/rm_old_sort_prefs.pl
@@ -4,13 +4,13 @@ use strict;
 use File::Spec::Functions qw(catdir updir);
 use FindBin;
 use lib catdir $FindBin::Bin, updir, 'lib';
-use Bric::Config qw(MASON_COMP_ROOT);
-use Bric::Util::Trans::FS;
+use bric_upgrade qw(:all);
 
 do_sql
-    q{DELETE from pref_opt WHERE value in ('id', 'category_name')},
+    q{DELETE from pref_opt WHERE value IN ('id', 'category_name')},
     q{UPDATE pref_opt SET description = 'Cover Date/Deploy Date'
       WHERE value = 'cover_date'},
     q{UPDATE pref_opt SET description = 'Document Type/Output Channel'
       WHERE value = 'element'},
+;
 
