b40ff46e358d903e95bf071c22097f9c1e28aa24
David Wheeler <david@justatheory.com>
2006-02-02 18:31:42 +0000

    Make 1.8.9 upgrades compatible with post 1.9.0.

--- inst/upgrade/lib/bric_upgrade.pm
+++ inst/upgrade/lib/bric_upgrade.pm
@@ -288,6 +288,7 @@ sub test_table($) {
   exit if test_column $table_name, $column_name, $min_size;
   exit if test_column $table_name, $column_name, undef, $not_null;
   exit if test_column $table_name, $column_name, $min_size, $not_null;
+  exit if test_column $table_name, $column_name, $min_size, $not_null, $type;
 
 This function returns true if the specified column exists in specified table
 in the Bricolage database, and false if it does not. Use C<test_column()> in
