aba3d84da27cfa97c46c1ce31a48bb6e8d6e13a0
David Wheeler <david@justatheory.com>
2006-03-23 17:16:37 +0000

    Duh! Really fix reverted docs.

--- inst/upgrade/1.10.1/fix_reverted_docs.pl
+++ inst/upgrade/1.10.1/fix_reverted_docs.pl
@@ -5,28 +5,28 @@ use File::Spec::Functions qw(catdir updir);
 use Bric::Util::DBI qw(:all);
 
 for my $doc (qw(story media)) {
-    my $sel = prepare('
+    my $sel = prepare("
         SELECT object_instance_id
-        FROM   story_element
+        FROM   $doc\_element
         WHERE  parent_id IS NULL
         GROUP BY object_instance_id
         HAVING COUNT(object_instance_id) > 1
-    ');
+    ");
 
     execute($sel);
     bind_columns($sel, \my $id);
 
-    my $sel_ids = prepare('
+    my $sel_ids = prepare("
         SELECT id
-        FROM   story_element
+        FROM   $doc\_element
         WHERE  object_instance_id = ?
                AND    parent_id IS NULL
-    ');
+    ");
 
-    my $del = prepare('
-        DELETE FROM story_element
+    my $del = prepare("
+        DELETE FROM $doc\_element
         WHERE  id = ?
-    ');
+    ");
 
     while (fetch($sel)) {
         my $ids = col_aref($sel_ids, $id);
