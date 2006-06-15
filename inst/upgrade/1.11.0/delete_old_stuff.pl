3b3272e004734a72dd4c9e358a2bc44e9c49a3b7
Marshall Roch <marshall@exclupen.com>
2006-06-15 08:03:19 +0000

    Deleted the 'New Organization' button, which we no longer need, and started a file to delete it and the rest of the crap I'll hopefully clear out this summer.

---
+++ inst/upgrade/1.11.0/delete_old_stuff.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl -w
+
+use strict;
+use File::Spec::Functions qw(catdir updir);
+use FindBin;
+use lib catdir $FindBin::Bin, updir, 'lib';
+use Bric::Config qw(MASON_COMP_ROOT);
+use Bric::Util::Trans::FS;
+
+my $fs = Bric::Util::Trans::FS->new;
+
+my @langs = qw(bo de_de en_us it_it km ko_ko lo my pt_pt ru_ru ug vi_vn zh_cn
+               zh_hk zh_tw);
+
+my @lang_dels = qw(
+    media/images/%s/new_organization_lgreen.gif
+);
+
+# Delete defunct language-specific UI components.
+for my $lang (@langs) {
+    for my $del (@lang_dels) {
+        $fs->del( $fs->cat_file(
+            MASON_COMP_ROOT->[0][1],
+            split '/', sprintf($del, $lang)
+        ) );
+    }
+}
