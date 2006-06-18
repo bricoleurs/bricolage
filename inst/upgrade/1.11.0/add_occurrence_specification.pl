613ffb007e1f4a29943eda123c0fd613a1413f61
Christian Muise <christian.muise@gmail.com>
2006-06-18 02:12:48 +0000

    Fixed to drop the old columns and add the new ones properly

--- inst/upgrade/1.11.0/add_occurrence_specification.pl
+++ inst/upgrade/1.11.0/add_occurrence_specification.pl
@@ -8,11 +8,14 @@ use bric_upgrade qw(:all);
 
 exit if fetch_sql q{SELECT 1 FROM field_type WHERE key_name = 'max_occurrence'};
 
-do_sql q{ALTER TABLE field_type ADD max_occurrence INTEGER},
+do_sql q{ALTER TABLE field_type ADD max_occurrence INTEGER NOT NULL},
        q{UPDATE field_type SET max_occurrence='0' WHERE quantifier='1'},
        q{UPDATE field_type SET max_occurrence='1' WHERE quantifier='0'},
 
-       q{ALTER TABLE field_type ADD min_occurrence INTEGER},
+       q{ALTER TABLE field_type ADD min_occurrence INTEGER NOT NULL},
        q{UPDATE field_type SET min_occurrence='0' WHERE required='0'},
-       q{UPDATE field_type SET min_occurrence='1' WHERE required='1'}; 
+       q{UPDATE field_type SET min_occurrence='1' WHERE required='1'},
+
+       q{ALTER TABLE field_type DROP COLUMN quantifier},
+       q{ALTER TABLE field_type DROP COLUMN required};
 
