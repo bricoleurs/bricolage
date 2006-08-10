d41a1edc10a96786eb7c9ab1e32d557e27456955
Christian Muise <christian.muise@gmail.com>
2006-08-10 13:47:43 +0000

    - Revised the upgrade script for subelements. - Rehashed the ElementTypes.val file for the subelements table (removed the uneeded group stuff)

--- inst/upgrade/1.11.0/add_occurrence_specification.pl
+++ inst/upgrade/1.11.0/add_occurrence_specification.pl
@@ -6,6 +6,7 @@ use FindBin;
 use lib catdir $FindBin::Bin, updir, 'lib';
 use bric_upgrade qw(:all);
 
+# Modify tables for field type occurrence
 exit if fetch_sql q{SELECT 1 FROM field_type WHERE key_name = 'max_occurrence'};
 
 do_sql q{ALTER TABLE field_type ADD max_occurrence INTEGER},
@@ -26,8 +27,13 @@ do_sql q{ALTER TABLE field_type ADD max_occurrence INTEGER},
        q{ALTER TABLE field_type ALTER COLUMN min_occurrence SET DEFAULT 0},
 ;
 
-# Add the table for subelements
-do_sql q{CREATE TABLE subelement_type  (
+# Modify tables for subelement occurrence
+exit if test_column 'element_type', 'parent_id';
+
+do_sql
+  q{CREATE SEQUENCE seq_subelement_type START 1024},
+
+  q{CREATE TABLE subelement_type  (
     id              INTEGER        NOT NULL
                                    DEFAULT NEXTVAL('seq_subelement_type'),
     parent_id       INTEGER        NOT NULL,
@@ -37,15 +43,36 @@ do_sql q{CREATE TABLE subelement_type  (
     max_occurrence  INTEGER        NOT NULL DEFAULT 0,
     CONSTRAINT pk_subelement_type__id PRIMARY KEY (id)
 )},
-       q{CREATE INDEX fkx_element_type__subelement__parent_id ON subelement_type(parent_id)},
-       q{CREATE INDEX fkx_element_type__subelement__child_id ON subelement_type(child_id)},
-       q{CREATE UNIQUE INDEX udx_subelement_type__parent__child ON subelement_type(parent_id, child_id)},
 
-       q{ALTER TABLE subelement_type ADD
+  q{ALTER TABLE subelement_type ADD
     CONSTRAINT fkx_element_type__subelement__parent_id FOREIGN KEY (parent_id)
     REFERENCES element_type(id) ON DELETE CASCADE},
 
-       q{ALTER TABLE subelement_type ADD
+  q{ALTER TABLE subelement_type ADD
     CONSTRAINT fkx_element_type__subelement__child_id FOREIGN KEY (child_id)
     REFERENCES element_type(id) ON DELETE CASCADE},
+    
+  q{CREATE INDEX fkx_element_type__subelement__parent_id ON subelement_type(parent_id)},
+  q{CREATE INDEX fkx_element_type__subelement__child_id ON subelement_type(child_id)},
+  q{CREATE UNIQUE INDEX udx_subelement_type__parent__child ON subelement_type(parent_id, child_id)},
 ;
+
+my $sel = prepare(q{
+    SELECT et.id AS parent_id, etm.object_id AS child_id
+    FROM   element_type et, member m, element_type_member etm
+    WHERE  et.et_grp__id = m.grp__id
+            AND m.id = etm.member__id
+    ORDER BY parent_id, child_id
+});
+
+my $ins = prepare(q{
+    INSERT INTO subelement_type (parent_id, child_id, place, min_occurrence, max_occurrence)
+    VALUES (?, ?, ?, 0, 0)
+});
+
+execute($sel);
+bind_columns($sel, \my ($parent_id, $child_id));
+my %seen;
+while (fetch($sel)) {
+    execute($ins, $parent_id, $child_id, ++$seen{$parent_id});
+}
