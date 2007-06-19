c52a413a4bde4016d7e72302ab52ad4c36ff303a
Scott Lanning <slanning@cpan.org>
2007-06-19 15:17:44 +0000

    fix bug 1283, adds <= to Alerts

--- lib/Bric/Util/AlertType.pm
+++ lib/Bric/Util/AlertType.pm
@@ -161,7 +161,7 @@ my $safe = Safe->new;
 # If new operators get added to Bric::Util::AlertType::Parts::Rule, use
 # `perl -MOpcode=opdump -e opdump` to find their ops and add them here.
 # XXX Add regcmaybe, regcreset, pushre, or regcomp?
-$safe->permit_only(qw(seq sne sgt slt sge le match not lc padany lineseq
+$safe->permit_only(qw(seq sne sgt slt sle sge le match not lc padany lineseq
                       const leaveeval qr regcreset));
 
 ################################################################################
