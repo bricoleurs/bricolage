51b8ad96081a3c8499f088271ff4a2f6adc6a1d2
Scott Lanning <slanning@cpan.org>
2008-03-06 09:50:29 +0000

    Patch from Alex Krohn to fix "ident" authentication. Everybody place your bets whether it's really fixed now. ^.^

--- inst/db_grant.pl
+++ inst/db_grant.pl
@@ -14,8 +14,9 @@ do $PGCONF or die "Failed to read $PGCONF : $!";
 if (my $sys_user = $PG->{system_user}) {
     print "Becoming $sys_user...\n";
     $> = $PG->{system_user_uid};
-    die "Failed to switch EUID to $PG->{system_user_uid} ($sys_user).\n"
-        unless $> == $PG->{system_user_uid};
+    $< = $PG->{system_user_uid};
+    die "Failed to switch EUID and RUID to $PG->{system_user_uid} ($sys_user).\n"
+        unless $> == $PG->{system_user_uid} and $< == $PG->{system_user_uid};
 }
 
 # Set environment variables for psql.
