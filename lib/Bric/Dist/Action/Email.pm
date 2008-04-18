dfc966e9a3ee36dd685b9972bb7e6676435209b3
David Wheeler <david@justatheory.com>
2008-04-18 00:14:49 +0000

    Fixed subroutein redefined warnings. Now all attributes in Bric::Dist::Action::Email normalize line endings.

--- lib/Bric/Dist/Action/Email.pm
+++ lib/Bric/Dist/Action/Email.pm
@@ -88,8 +88,7 @@ use constant IGNORE => 3;
 ################################################################################
 # Instance Fields
 BEGIN {
-    Bric::register_fields
-      ({
+    Bric::register_fields({
         # Public Fields.
         from         => Bric::FIELD_READ,
         to           => Bric::FIELD_READ,
@@ -100,7 +99,7 @@ BEGIN {
         content_type => Bric::FIELD_READ,
         handle_text  => Bric::FIELD_READ,
         handle_other => Bric::FIELD_READ,
-       });
+    });
 }
 
 ##############################################################################
@@ -186,8 +185,8 @@ Determines how non-text resources are to be handled.
     # We don't got 'em. So get 'em!
     my ($meths, @ord);
     foreach my $meth (__PACKAGE__->SUPER::my_meths(1)) {
-	$meths->{$meth->{name}} = $meth;
-	push @ord, $meth->{name};
+        $meths->{$meth->{name}} = $meth;
+        push @ord, $meth->{name};
     }
 
     push @ord, qw(from to cc bcc subject content_type handle_text
@@ -292,11 +291,11 @@ Determines how non-text resources are to be handled.
                               len      => 256,
                               req      => 1,
                               type     => 'short',
-			      props    => { type => 'select',
-					    vals => [[INLINE, 'Inline'],
-						     [ATTACH, 'Attach'],
-						     [IGNORE, 'Ignore']],
-					  }
+                  props    => { type => 'select',
+                        vals => [[INLINE, 'Inline'],
+                             [ATTACH, 'Attach'],
+                             [IGNORE, 'Ignore']],
+                      }
                             };
 
     $meths->{handle_other} = { get_meth => sub { shift->get_handle_other(@_) },
@@ -336,7 +335,8 @@ Bric::Dist::Action.
   my $from = $action->get_from;
   $action = $action->set_from($from);
 
-Get and set the address from which email will be sent. Optional.
+Get and set the address from which email will be sent. Optional. The setter
+converts non-Unix line endings.
 
 =head3 to
 
@@ -347,14 +347,6 @@ Get and set the address or addresses to which email will be sent. Multiple
 addresses should be separated by commas. Either C<to> or C<bcc> or both are
 required. The setter converts non-Unix line endings.
 
-=cut
-
-sub set_to {
-    my ($self, $val) = @_;
-    $val =~ s/\r\n?/\n/g if defined $val;
-    $self->_set( [ 'to' ] => [ $val ]);
-}
-
 =head3 cc
 
   my $cc = $action->get_cc;
@@ -363,14 +355,6 @@ sub set_to {
 Get and set the address or addresses to which email will be Cc'd. Multiple
 addresses should be separated by commas. Optional. The setter converts
 non-Unix line endings.
-
-=cut
-
-sub set_cc {
-    my ($self, $val) = @_;
-    $val =~ s/\r\n?/\n/g if defined $val;
-    $self->_set( [ 'cc' ] => [ $val ]);
-}
 
 =head3 bcc
 
@@ -381,20 +365,13 @@ Get and set the address or addresses to which email will be Bcc'd. Multiple
 addresses should be separated by commas. Either C<to> or C<bcc> or both are
 required. The setter converts non-Unix line endings.
 
-=cut
-
-sub set_bcc {
-    my ($self, $val) = @_;
-    $val =~ s/\r\n?/\n/g if defined $val;
-    $self->_set( [ 'bcc' ] => [ $val ]);
-}
-
 =head3 subject
 
   my $subject = $action->get_subject;
   $action = $action->set_subject($subject);
 
-Get and set the subject to be used when emails are sent. Optional.
+Get and set the subject to be used when emails are sent. Optional. The setter
+converts non-Unix line endings.
 
 =head3 content_type
 
@@ -403,7 +380,7 @@ Get and set the subject to be used when emails are sent. Optional.
 
 Get and set the content type to be used when emails are sent. If not
 specified, Bric::Dist::Action::Email will use the media type of the first text
-file it uses for the email message.
+file it uses for the email message. The setter converts non-Unix line endings.
 
 =head3 handle_text
 
@@ -432,6 +409,8 @@ Ignore text resources.
 
 =back
 
+The setter converts non-Unix line endings.
+
 =head3 handle_other
 
   my $handle_other = $action->get_handle_other;
@@ -453,6 +432,8 @@ Ignore text resources. The default value.
 Attach all text files to the email message.
 
 =back
+
+The setter converts non-Unix line endings.
 
 =cut
 
@@ -514,16 +495,16 @@ sub do_it {
 
     # Just bail if there's nothing to send.
     return $self unless $msg || @attach;
-    my $mailer = Bric::Util::Trans::Mail->new
-      ({ to           => [ $self->$get_attr('to') ],
-         from         => $self->$get_attr('from'),
-         cc           => [ $self->$get_attr('cc') ],
-         bcc          => [ $self->$get_attr('bcc') ],
-         content_type => $content_type,
-         subject      => $self->$get_attr('subject'),
-         message      => $msg,
-         resources    => \@attach,
-       });
+    my $mailer = Bric::Util::Trans::Mail->new({
+        to           => [ $self->$get_attr('to') ],
+        from         => $self->$get_attr('from'),
+        cc           => [ $self->$get_attr('cc') ],
+        bcc          => [ $self->$get_attr('bcc') ],
+        content_type => $content_type,
+        subject      => $self->$get_attr('subject'),
+        message      => $msg,
+        resources    => \@attach,
+    });
 
     $mailer->send;
     return $self;
@@ -595,13 +576,18 @@ $get_attr = sub {
     my ($self, $key) = (shift, shift);
     my $attr = $self->_get_attr;
     if (@_) {
-	$attr->set_attr({ name     => $key,
-                          subsys   => 'EmailAction',
-			  sql_type => 'blob',
-                          value    => shift });
+        (my $val = shift) =~ s/\r\n?/\n/g;
+        $attr->set_attr({
+            name     => $key,
+            subsys   => 'EmailAction',
+            sql_type => 'blob',
+            value    => $val,
+        });
     } else {
-	return $attr->get_attr({ name   => $key,
-                                 subsys => 'EmailAction' });
+        return $attr->get_attr({
+            name   => $key,
+            subsys => 'EmailAction',
+        });
     }
     return $self;
 };
@@ -648,7 +634,7 @@ each destination for which resources are to be distributed.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2003 Kineticode, Inc. See L<Bric::License|Bric::License> for
-complete license terms and conditions.
+Copyright (c) 2003-2008 Kineticode, Inc. See L<Bric::License|Bric::License>
+for complete license terms and conditions.
 
 =cut
