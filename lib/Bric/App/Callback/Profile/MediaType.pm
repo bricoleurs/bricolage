32acf24493a8e0a9508ffd3bc29b763e9fab6dee
David Wheeler <david@justatheory.com>
2008-04-29 05:48:49 +0000

    Greatly simplified the processing in Bric::App::Callback::Profile::MediaType. It was too complex, and didn't work properly with the Ajax extension fields.

--- lib/Bric/App/Callback/Profile/MediaType.pm
+++ lib/Bric/App/Callback/Profile/MediaType.pm
@@ -15,7 +15,6 @@ my $type = 'media_type';
 my $disp_name = 'Media Type';
 my $class = 'Bric::Util::MediaType';
 
-
 sub save : Callback {
     my $self = shift;
 
@@ -60,56 +59,33 @@ sub save : Callback {
         }
 
         # Process add_more widget.
-        my (@old_exts, @new_exts, $mtids, $used_ext, $addext_sub);
-        @old_exts = $mt->get_exts();
-        $mtids = mk_aref($param->{media_type_ext_id});
-        $used_ext = 0;
-        $addext_sub = sub {
-            my ($mt, $extension, $name) = @_;
-            unless ($extension =~ /^\s*$/) {
-                if ($extension =~ /^\w{1,10}$/) {
-                    my $mt2_id = Bric::Util::MediaType->get_id_by_ext($extension);
-                    if (defined $mt2_id && $mt2_id != $mt_id) {
-                        $used_ext ||= 1;
-                        add_msg('Extension "[_1]" is already used by media type "[_2]".',
-                                $extension,
-                                Bric::Util::MediaType->get_name_by_ext($extension));
-                    } else {
-                        my @addexts = @{[$extension]};
-                        unless ($mt->add_exts(@addexts)) {
-                            add_msg('Problem adding "[_1]"', "@addexts");
-                        }
-                    }
-                } else {
-                    add_msg('Extension "[_1]" ignored.', $extension);
-                }
+        my $used_ext = 0;
+        my %old_exts = map { $_ => 1 } my @orig_exts = $mt->get_exts;
+        for my $extension (@{ mk_aref( $param->{extension} ) } ) {
+            next unless $extension && $extension !~ /^\s+$/;
+            next if delete $old_exts{ lc $extension };
+            if ($extension !~ /^\w{1,10}$/) {
+                add_msg('Extension "[_1]" ignored.', $extension);
+                next;
             }
-            return;
-        };
-
-        $used_ext = 0;
-        my $exts = mk_aref( $param->{extension} );
-        for (my $i = 0; $i < @{ $exts }; $i++) {
-            if (my $ext = $mtids->[$i]) {
-                my @delexts = @{[$ext]};
-                unless ($mt->del_exts(@delexts)) {
-                    add_msg('Problem deleting "[_1]"', "@delexts");
-                }
-                my $extension = $exts->[$i];
-                $used_ext += $addext_sub->($mt, $extension, $name);
+            my $mt_id = Bric::Util::MediaType->get_id_by_ext($extension);
+            if ( $mt_id && $mt_id != $mt->get_id ) {
+                add_msg(
+                    'Extension "[_1]" is already used by media type "[_2]".',
+                    $extension,
+                    Bric::Util::MediaType->get_name_by_ext($extension)
+                );
+                $used_ext += 1;
             } else {
-                next unless $exts->[$i];
-                my $extension = $exts->[$i];
-                $used_ext += $addext_sub->($mt, $extension, $name);
+                add_msg('Problem adding "[_1]"', $extension)
+                    unless $mt->add_exts($extension);
             }
         }
-        if ($param->{del_media_type_ext}) {
-            $mt->del_exts(@{ mk_aref($param->{del_media_type_ext}) });
-        }
-        @new_exts = $mt->get_exts();
-        unless (@new_exts) {
+
+        $mt->del_exts( keys %old_exts ) if %old_exts;
+        unless (($mt->get_exts)[0]) {
             # Revert the extensions
-            $mt->add_exts(@old_exts);
+            $mt->add_exts(@orig_exts);
             add_msg('At least one extension is required.');
             $used_ext = 1;
         }
