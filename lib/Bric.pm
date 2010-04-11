19392938747f8548ff2814d039c9bc9a1b6c6778
David E. Wheeler <david@justatheory.com>
2010-04-11 14:24:15 -0700

    Increment version number to 1.10.11.

--- lib/Bric.pm
+++ lib/Bric.pm
@@ -6,13 +6,13 @@ Bric - The Bricolage base class.
 
 =head1 Release Version
 
-1.10.10
+1.10.11
 
 =cut
 
 # Set the Version Number.
 BEGIN {
-    our $VERSION = '1.10.10';
+    our $VERSION = '1.10.11';
 }
 
 =head1 SYNOPSIS
