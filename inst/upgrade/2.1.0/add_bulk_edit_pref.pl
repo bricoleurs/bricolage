4c71a69a67bc3c0f68ea713fb54155d6cb0eb2c7
David E. Wheeler <david@justatheory.com>
2011-04-05 12:06:25 -0700

    Add "Show Bulk Edit" preference.
    When turned off, the "Bulk Edit" button no longer appears in the container
    element interface. Useful for organizations where users don't know or care
    about bulk editing, and the button might otherwise confuse them. As with all
    preferences, its setting can be overridden for individual users. Sponsored by
    Denision University.

---
+++ inst/upgrade/2.1.0/add_bulk_edit_pref.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl -w
+
+use strict;
+use File::Spec::Functions qw(catdir updir);
+use FindBin;
+use lib catdir $FindBin::Bin, updir, 'lib';
+use bric_upgrade qw(:all);
+
+exit if fetch_sql q{SELECT 1 FROM pref WHERE id = 19};
+
+do_sql q{
+    INSERT INTO pref (id, name, description, value, def, manual, opt_type)
+    VALUES ('19', 'Show Bulk Edit', 'Show button t bulk edit document elements.',
+            '1', '1', '0', 'select')},
+
+    q{INSERT INTO member (id, grp__id, class__id, active)
+    VALUES ('905', '22', '48', '1')},
+
+    q{INSERT INTO pref_member (id, object_id, member__id)
+    VALUES ('19', '19', '905')},
+
+    q{INSERT INTO pref_opt (pref__id, value, description)
+    VALUES ('19', '0', 'Off')},
+
+    q{INSERT INTO pref_opt (pref__id, value, description)
+    VALUES ('19', '1', 'On')},
+;
+
+
