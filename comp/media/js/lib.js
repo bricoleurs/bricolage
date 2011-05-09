77211b70c872365c7d467b0c5f8b56f3a51289a4
Bret Dawson <bret@pectopah.com>
2011-05-09 13:15:52 -0400

    Bringing local repo up to date.

--- comp/media/js/lib.js
+++ comp/media/js/lib.js
@@ -1602,6 +1602,7 @@ var Container = {
                 Container.updateOrder(elem);
             },
             handle: 'name',
+            only: ['container', 'element'],
             scroll: window
         });
 
@@ -1640,7 +1641,7 @@ var Container = {
             '/widgets/container_prof/_related.html', {
                 parameters: 'type=' + type + '&widget=' + widget +
                     '&container_id=' + container_id + '&container_prof|' +
-                     action + '_' + type + '_cb=' + asset_id,
+                     action + '_' + type + '_cb=' + asset_id + '&displayed=1',
                 asynchronous: !sync,
                 onSuccess: function(r) { window.close(); },
                 onFailure: Bricolage.handleError,
@@ -1669,7 +1670,7 @@ var Container = {
            },
            '/widgets/container_prof/_related.html', {
                parameters: 'type=' + type + '&widget=' + widget +
-                   '&container_id=' + container_id,
+                   '&container_id=' + container_id + '&displayed=1',
                asynchronous: !sync,
                onSuccess: function(r) { window.close(); },
                onFailure: Bricolage.handleError,
@@ -1749,8 +1750,61 @@ var Container = {
         if (hint_element) {
             Effect.toggle(hint_element, 'blind', {duration: 0.3});
         }
+        Effect.toggle(
+            'element_' + eid + '_hr',
+            'blind',
+            {duration: 0.3}
+        );
         return false;
-    }
+    },
+
+    toggle_related: function( type, eid, anchor ) {
+        var displayed = $('container_' + eid + '_' + type + '_displayed');
+        var thumb = $('container_' + eid + '_rel_thumb');
+        var data  = $('container_' + eid + '_rel_' + type + '_data');
+        var thumbClass;
+        var dataClass;
+        if ( displayed.value == '0' ) {
+            // Display it.
+            anchor.innerHTML = '&#x25bc;';
+            displayed.value = '1';
+            thumbClass = 'thumb';
+            dataClass = 'reldata';
+        } else {
+            // Hide it.
+            anchor.innerHTML = '&#x25b6;';
+            displayed.value = '0';
+            thumbClass = 'smallthumb';
+            dataClass = 'smallreldata';
+        }
+        if (thumb != null) thumb.className = thumbClass;
+        if (data != null) data.className = dataClass;
+
+        Effect.toggle(
+            'container_' + eid + '_rel_' + type + '_shortsum',
+            'blind',
+            {duration: 0.3}
+        );
+        Effect.toggle(
+            'container_' + eid + '_rel_' + type + '_summary',
+            'blind',
+            {duration: 0.3}
+        );
+        return false;
+    },
+
+    find_related: function ( eid, type, rel_type ) {
+        openWindow(
+            '/workflow/profile/' + type + '/container/edit_related_' + rel_type + '.html?id=' + eid,
+            'RelatedBricolagePopup',
+            { closeOnUnload: true, width: 780 }
+        );
+        var displayed = $('container_' + eid + '_' + rel_type + '_displayed');
+        if (displayed.value == '0') {
+            this.toggle_related(rel_type, eid, $(rel_type + '_' + eid + '_showhide'));
+        }
+        return false;
+    },
 };
 
 Event.observe(window, 'load', function() {
